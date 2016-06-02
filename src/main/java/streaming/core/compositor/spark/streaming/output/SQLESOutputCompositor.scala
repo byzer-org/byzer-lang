26c2dbf5103926b9c966460ddec8d6466ff1a29c
WilliamZhu <allwefantasy@gmail.com>
2016-06-02 16:29:37 +0800

    add compositor which convert Java map or Scala map to json string

--- src/main/java/streaming/core/compositor/spark/streaming/output/SQLESOutputCompositor.scala
+++ src/main/java/streaming/core/compositor/spark/streaming/output/SQLESOutputCompositor.scala
@@ -1,12 +1,15 @@
 package streaming.core.compositor.spark.streaming.output
 
 import java.util
+import java.util.Locale
 
 import org.apache.log4j.Logger
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.streaming.dstream.DStream
 import org.elasticsearch.spark.sql._
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import serviceframework.dispatcher.{Compositor, Processor, Strategy}
 import streaming.core.compositor.spark.streaming.CompositorHelper
 import streaming.core.strategy.ParamsValidator
@@ -27,22 +30,34 @@ class SQLESOutputCompositor[T] extends Compositor[T] with CompositorHelper with 
   }
 
   def resource = {
-    config[String]("resource", _configParams)
+    val tmp = config[String]("resource", _configParams)
+    if (!tmp.isDefined) config[String]("es.resource", _configParams)
+    else tmp
   }
 
   def cfg = {
     val _cfg = _configParams(0).map(f => (f._1.asInstanceOf[String], f._2.asInstanceOf[String])).toMap
-    _cfg - "resource"
+    _cfg - "resource" - "timeFormat"
+
+  }
+
+
+  def timeformat = {
+    config[String]("timeFormat", _configParams)
   }
 
   override def result(alg: util.List[Processor[T]], ref: util.List[Strategy[T]], middleResult: util.List[T], params: util.Map[Any, Any]): util.List[T] = {
     val dstream = middleResult.get(0).asInstanceOf[DStream[String]]
     val func = params.get("_func_").asInstanceOf[(RDD[String]) => DataFrame]
     val _resource = resource.get
+    val _timeFormat = timeformat.getOrElse("")
     val _cfg = cfg
     dstream.foreachRDD { rdd =>
       val df = func(rdd)
-      df.saveToEs(_resource, _cfg)
+      val fmt = DateTimeFormat.forPattern(_timeFormat).withLocale(Locale.ENGLISH)
+      val timeStamp = new DateTime(fmt).toString(fmt)
+      val finalResource = if (timeformat.isEmpty) _resource else _resource + "_" + timeStamp
+      df.saveToEs(finalResource, _cfg)
     }
     params.remove("sql")
     new util.ArrayList[T]()
