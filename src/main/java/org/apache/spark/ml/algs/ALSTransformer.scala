edd004f7a0ad35bf6b42f45ef45cdd74bfafe147
WilliamZhu <allwefantasy@gmail.com>
2016-07-28 14:45:28 +0800

    tmp

--- src/main/java/org/apache/spark/ml/algs/ALSTransformer.scala
+++ src/main/java/org/apache/spark/ml/algs/ALSTransformer.scala
@@ -8,13 +8,22 @@ import org.apache.spark.sql.DataFrame
 /**
  * 7/28/16 WilliamZhu(allwefantasy@gmail.com)
  */
-class ALSTransformer(path: String,parameters:Map[String,String]) extends BaseAlgorithmTransformer{
+class ALSTransformer(path: String, parameters: Map[String, String]) extends BaseAlgorithmTransformer {
 
   val model = ALSModel.load(path)
+  val matrixFactorizationModel = new MatrixFactorizationModel(model.rank, convertToRDD(model.userFactors), convertToRDD(model.itemFactors))
 
   def transform(df: DataFrame): DataFrame = {
+    if(parameters.contains("recommendByUser")){
+      matrixFactorizationModel.recommendProducts()
+    }
     model.transform(df)
-    //new MatrixFactorizationModel(model.rank,model.itemFactors,model.userFactors)
+  }
+
+  private def convertToRDD(dataFrame: DataFrame) = {
+    dataFrame.select("id", "features").map { case (id: Int, features: Array[Float]) =>
+      (id, features.map(_.toDouble).array)
+    }
   }
 
 }
