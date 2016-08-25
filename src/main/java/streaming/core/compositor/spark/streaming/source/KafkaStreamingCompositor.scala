8a0d8673c94fc1fb96b75405c1bf5918dc708760
WilliamZhu <allwefantasy@gmail.com>
2016-08-25 15:57:33 +0800

    add kafka receiver based mode support

--- src/main/java/streaming/core/compositor/spark/streaming/source/KafkaStreamingCompositor.scala
+++ src/main/java/streaming/core/compositor/spark/streaming/source/KafkaStreamingCompositor.scala
@@ -28,6 +28,16 @@ class KafkaStreamingCompositor[T] extends Compositor[T] {
     }.toMap.asInstanceOf[Map[String, String]]
   }
 
+  private def zkEnable = {
+    getKafkaParams.containsKey("zk") ||
+      getKafkaParams.containsKey("zookeeper") ||
+      getKafkaParams.containsKey("zkQuorum")
+  }
+
+  private def getZk = {
+    getKafkaParams.getOrElse("zk", getKafkaParams.getOrElse("zookeeper", getKafkaParams.getOrElse("zkQuorum", "127.0.0.1")))
+  }
+
   private def getTopics = {
     _configParams.get(0).get("topics").asInstanceOf[String].split(",").toSet
   }
@@ -35,10 +45,19 @@ class KafkaStreamingCompositor[T] extends Compositor[T] {
   override def result(alg: util.List[Processor[T]], ref: util.List[Strategy[T]], middleResult: util.List[T], params: util.Map[Any, Any]): util.List[T] = {
     val runtime = params.get("_runtime_").asInstanceOf[SparkStreamingRuntime]
     val ssc = runtime.streamingContext
-    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc,
-      getKafkaParams,
-      getTopics)
+
+    val kafkaStream = if (zkEnable) {
+      val zk = getZk
+      val groupId = getKafkaParams.get("groupId").get
+      val topics = getTopics.map(f => (f, 1)).toMap
+      KafkaUtils.createStream(ssc, zk, groupId, topics)
+    } else {
+      KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+        ssc,
+        getKafkaParams,
+        getTopics)
+    }
+
     val tempStream = kafkaStream.map(f => f._2)
     List(tempStream.asInstanceOf[T])
   }
