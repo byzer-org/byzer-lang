73beac5c580c6538760c142c9baa4a85af280181
WilliamZhu <allwefantasy@gmail.com>
2018-01-13 16:16:13 +0800

    support word2vec params configure with where condtition

--- streamingpro-spark-2.0/src/main/java/streaming/dsl/mmlib/algs/SQLWord2Vec.scala
+++ streamingpro-spark-2.0/src/main/java/streaming/dsl/mmlib/algs/SQLWord2Vec.scala
@@ -1,5 +1,7 @@
 package streaming.dsl.mmlib.algs
 
+import java.lang.reflect.Method
+
 import org.apache.spark.ml.feature.{Word2Vec, Word2VecModel}
 import org.apache.spark.ml.linalg.DenseVector
 import org.apache.spark.ml.util.{MLWritable, MLWriter}
@@ -17,8 +19,22 @@ class SQLWord2Vec extends SQLAlg {
 
   def train(df: DataFrame, path: String, params: Map[String, String]) = {
     val w2v = new Word2Vec()
-    w2v.setInputCol(params("inputCol"))
-    w2v.setMinCount(0)
+    w2v.params.map { f =>
+      if (params.containsKey(f.name)) {
+        val v = params(f.name)
+        val m = w2v.getClass.getMethods.filter(m => m.getName == s"set${f.name.capitalize}").head
+        val pt = m.getParameterTypes.head
+        val v2 = pt match {
+          case i if i.isAssignableFrom(classOf[Int])=> v.toInt
+          case i if i.isAssignableFrom(classOf[Double]) => v.toDouble
+          case i if i.isAssignableFrom(classOf[Float]) => v.toFloat
+          case _ => v
+        }
+        m.invoke(w2v, v2.asInstanceOf[AnyRef])
+      }
+    }
+    //    w2v.setInputCol(params("inputCol"))
+    //    w2v.setMinCount(0)
     val model = w2v.fit(df)
     model.write.overwrite().save(path)
   }
