ccd27b3feee7478dad63488d3e1a70d1bf0bcd74
WilliamZhu <allwefantasy@gmail.com>
2018-04-16 16:04:36 +0800

    支持流式xql

--- streamingpro-spark-2.0/src/main/java/streaming/dsl/DslAdaptor.scala
+++ streamingpro-spark-2.0/src/main/java/streaming/dsl/DslAdaptor.scala
@@ -5,9 +5,11 @@ import streaming.dsl.parser.DSLSQLParser.SqlContext
 /**
   * Created by allwefantasy on 27/8/2017.
   */
-trait DslAdaptor {
+trait DslAdaptor extends DslTool {
   def parse(ctx: SqlContext): Unit
+}
 
+trait DslTool {
   def cleanStr(str: String) = {
     if (str.startsWith("`") || str.startsWith("\""))
       str.substring(1, str.length - 1)
