a2d4e5fd04fc2f338efa6fcaaed19535f76f667e
WilliamZhu <allwefantasy@gmail.com>
2018-05-24 11:09:18 +0800

    支持词向量模型

--- streamingpro-spark-2.0/src/main/java/streaming/dsl/mmlib/algs/SQLWord2VecInPlace.scala
+++ streamingpro-spark-2.0/src/main/java/streaming/dsl/mmlib/algs/SQLWord2VecInPlace.scala
@@ -1,19 +1,20 @@
 package streaming.dsl.mmlib.algs
 
-import java.util.UUID
-
+import MetaConst._
 import org.apache.spark.sql.expressions.UserDefinedFunction
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.types.{ArrayType, DoubleType, StringType, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession, functions => F}
+import streaming.core.shared.SharedObjManager
 import streaming.dsl.mmlib.SQLAlg
 import streaming.dsl.mmlib.algs.feature.StringFeature
-import org.apache.spark.sql.{functions => F}
+import streaming.dsl.mmlib.algs.meta.Word2VecMeta
 
 /**
   * Created by allwefantasy on 7/5/2018.
   */
 class SQLWord2VecInPlace extends SQLAlg with Functions {
   override def train(df: DataFrame, path: String, params: Map[String, String]): Unit = {
-    interval_train(df, params).write.mode(SaveMode.Overwrite).parquet(path)
+    interval_train(df, params + ("path" -> path)).write.mode(SaveMode.Overwrite).parquet(getDataPath(path))
   }
 
   def interval_train(df: DataFrame, params: Map[String, String]) = {
@@ -21,10 +22,12 @@ class SQLWord2VecInPlace extends SQLAlg with Functions {
     val inputCol = params.getOrElse("inputCol", "")
     val stopWordPath = params.getOrElse("stopWordPath", "")
     val flat = params.getOrElse("flatFeature", "true").toBoolean
-    require(!inputCol.isEmpty, "inputCol is required when use SQLTfIdfInPlace")
+    require(!inputCol.isEmpty, "inputCol is required when use SQLWord2VecInPlace")
+    val metaPath = getMetaPath(params("path"))
+    // keep params
+    saveTraningParams(df.sparkSession, params, metaPath)
 
-    val mappingPath = if (params.contains("mappingPath")) params("mappingPath") else ("/tmp/" + UUID.randomUUID().toString)
-    var newDF = StringFeature.word2vec(df, mappingPath, dicPaths, inputCol, stopWordPath)
+    var newDF = StringFeature.word2vec(df, metaPath, dicPaths, inputCol, stopWordPath)
     if (flat) {
       val flatFeatureUdf = F.udf((a: Seq[Seq[Double]]) => {
         a.flatMap(f => f)
@@ -34,7 +37,63 @@ class SQLWord2VecInPlace extends SQLAlg with Functions {
     newDF
   }
 
-  override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = ???
+  override def load(spark: SparkSession, _path: String, params: Map[String, String]): Any = {
+    import spark.implicits._
+    //load train params
+    val path = getMetaPath(_path)
+    val df = spark.read.parquet(PARAMS_PATH(path, "params")).map(f => (f.getString(0), f.getString(1)))
+    val trainParams = df.collect().toMap
+    val inputCol = trainParams.getOrElse("inputCol", "")
+    //load wordindex
+    val wordIndex = spark.read.parquet(WORD_INDEX_PATH(path, inputCol)).map(f => ((f.getString(0), f.getDouble(1)))).collect().toMap
+    //load word2vec model
+    val word2vec = new SQLWord2Vec()
+    val model = word2vec.load(spark, WORD2VEC_PATH(path, inputCol), Map())
+    val predictFunc = word2vec.internal_predict(df.sparkSession, model, "wow")("wow_array").asInstanceOf[(Seq[String]) => Seq[Seq[Double]]]
+    Word2VecMeta(trainParams, wordIndex, predictFunc)
+  }
 
-  override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = ???
+  override def predict(spark: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = {
+    val word2vecMeta = _model.asInstanceOf[Word2VecMeta]
+    val trainParams = word2vecMeta.trainParams
+    val dicPaths = trainParams.getOrElse("dicPaths", "")
+    val stopWordPath = trainParams.getOrElse("stopWordPath", "")
+
+    val wordIndexBr = spark.sparkContext.broadcast(word2vecMeta.wordIndex)
+
+
+    val df = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], StructType(Seq()))
+    val stopwords = StringFeature.loadStopwords(df, stopWordPath)
+    val stopwordsBr = spark.sparkContext.broadcast(stopwords)
+
+    val func = (content: String) => {
+
+      // create analyser
+      val forest = SharedObjManager.getOrCreate[Any](dicPaths, SharedObjManager.forestPool, () => {
+        val words = SQLTokenAnalysis.loadDics(spark, trainParams)
+        SQLTokenAnalysis.createForest(words, trainParams)
+      })
+      val parser = SQLTokenAnalysis.createAnalyzerFromForest(forest.asInstanceOf[AnyRef], trainParams)
+      // analyser content
+      val wordArray = SQLTokenAnalysis.parseStr(parser, content, trainParams).
+        filter(f => !stopwordsBr.value.contains(f))
+
+      val wordIntArray = wordArray.filter(f => wordIndexBr.value.contains(f)).map(f => wordIndexBr.value(f).toInt)
+      wordIntArray.map(f => f.toString).toSeq
+    }
+
+    if (trainParams.getOrElse("flatFeature", "false").toBoolean) {
+      val f2 = (a: String) => {
+        word2vecMeta.predictFunc(func(a)).flatMap(f => f)
+      }
+      UserDefinedFunction(f2, ArrayType(DoubleType), Some(Seq(StringType)))
+    } else {
+      val f2 = (a: String) => {
+        word2vecMeta.predictFunc(func(a))
+      }
+      UserDefinedFunction(f2, ArrayType(ArrayType(DoubleType)), Some(Seq(StringType)))
+    }
+
+  }
+
 }
