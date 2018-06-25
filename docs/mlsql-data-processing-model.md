5d4466e9712f64b50d6510686a4d3c76245c89fc
伍和义 <wuheyi@dxy.cn>
2018-06-25 10:39:41 +0800

    [update] 根据代码review进行修改

--- docs/mlsql-data-processing-model.md
+++ docs/mlsql-data-processing-model.md
@@ -133,6 +133,7 @@ select word2vec(content) from sometable
 可参考[MLSQL 模型部署](https://github.com/allwefantasy/streamingpro/blob/master/docs/mlsql-model-deploy.md)
 
 
+
 ### ScalerInPlace
 
 特征尺度变换，这是对double类型字段做特征工程的一个算法。使用方法如下：
@@ -173,28 +174,6 @@ register ScalerInPlace.`/tmp/scaler` as jack;
 ```sql
 select jack(array(a,b))[0] a,jack(array(a,b))[1] b, c from orginal_text_corpus
 ```
-
-## ModelExplainInPlace
-加载sklearn模型，显示训练后的模型参数
-
-train traindataframe as ConfusionMatrix.`/Users/dxy_why/tmp/modelExplainInPlace/` 
-where `systemParam.pythonPath`="python"
-and `systemParam.pythonVer`="2.7"
-// 模型路径
-and `modelPath`="/tmp/svm.pickle"
-```
-模型参数保存在/Users/dxy_why/tmp/modelExplainInPlace/data
-```
-select  * from parquet.`/Users/dxy_why/tmp/modelExplainInPlace/data` 
-```
-
-
-参数使用说明：
-
-|参数|默认值|说明|
-|:----|:----|:----|
-|modelPath|""|模型路径|
-
 
 
 ## ConfusionMatrix
@@ -815,7 +794,27 @@ register DicOrTableToArray.`/tmp/model2` as p2;
 select p2("dic2")  as k
 ```
 
+### ModelExplainInPlace
+加载sklearn模型，显示训练后的模型参数
 
+train traindataframe as ModelExplainInPlace.`tmp/modelExplainInPlace/` 
+where `systemParam.pythonPath`="python"
+and `systemParam.pythonVer`="2.7"
+// 模型路径
+and `modelPath`="/tmp/svm.pickle"
+```
+模型参数保存在/tmp/modelExplainInPlace/data，
+该参数在ModelExplainInPlace.`tmp/modelExplainInPlace/` 处设置
+```
+select  * from parquet.`/tmp/modelExplainInPlace/data` 
+```
+
+
+参数使用说明：
+
+|参数|默认值|说明|
+|:----|:----|:----|
+|modelPath|""|模型路径|
 
 
 
--- streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLModelExplainInPlace.scala
+++ streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLModelExplainInPlace.scala
@@ -1,7 +1,12 @@
 package streaming.dsl.mmlib.algs
 
+import java.io.File
 import java.util
+import java.util.UUID
 
+import org.apache.commons.io.FileUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.ml.linalg.SQLDataTypes._
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.types._
@@ -20,28 +25,49 @@ class SQLModelExplainInPlace extends SQLAlg with Functions {
   override def train(df: DataFrame, path: String, params: Map[String, String]): Unit = {
     val systemParam = mapParams("systemParam", params)
     val pythonPath = systemParam.getOrElse("pythonPath", "python")
-    val paramMap = new util.HashMap[String, Object]()
-    paramMap.put("systemParam", systemParam.asJava)
-    paramMap.put("modelPath", params.getOrElse("modelPath", ""))
-    val tfName = "mlsql_sk_attributes.py"
-    val filePath = s"/python/${tfName}"
-    val tfSource = Source.fromInputStream(ExternalCommandRunner.getClass.getResourceAsStream(filePath)).
-      getLines().mkString("\n")
-    val pythonScript = PythonScript(tfName, tfSource, filePath)
-    val res = ExternalCommandRunner.run(Seq(pythonPath, pythonScript.fileName),
-      paramMap,
-      MapType(StringType, MapType(StringType, StringType)),
-      pythonScript.fileContent,
-      pythonScript.fileName, modelPath = null, kafkaParam = null,
-      validateData = null
-    )
-    res.foreach(f => f)
-    df.sparkSession.read.json("/tmp/attributes.json")
-      .write.mode(SaveMode.Overwrite).parquet(getDataPath(path))
 
+    import df.sparkSession.sqlContext.implicits._
+    val ExternalCommandRunnerDataframe = Seq("aaa").toDF("test")
+    ExternalCommandRunnerDataframe.rdd.map(f => {
+      val paramMap = new util.HashMap[String, Object]()
+      paramMap.put("systemParam", systemParam.asJava)
+      paramMap.put("modelPath", params.getOrElse("modelPath", ""))
+      val tempModelLocalPath = s"${SQLPythonFunc.getLocalBasePath}/${UUID.randomUUID().toString}/0"
+      FileUtils.forceMkdir(new File(tempModelLocalPath))
+
+      paramMap.put("internalSystemParam", Map(
+        "tempModelLocalPath" -> tempModelLocalPath
+      ).asJava)
+      val tfName = "mlsql_sk_attributes.py"
+      val filePath = s"/python/${tfName}"
+      val tfSource = Source.fromInputStream(ExternalCommandRunner.getClass.getResourceAsStream(filePath)).
+        getLines().mkString("\n")
+      val pythonScript = PythonScript(tfName, tfSource, filePath)
+
+      val res = ExternalCommandRunner.run(Seq(pythonPath, pythonScript.fileName),
+        paramMap,
+        MapType(StringType, MapType(StringType, StringType)),
+        pythonScript.fileContent,
+        pythonScript.fileName, modelPath = null, kafkaParam = null,
+        validateData = null
+      )
+      //模型保存到hdfs上
+      val fs = FileSystem.get(new Configuration())
+      val modelHDFSPath = SQLPythonFunc.getAlgModelPath(path) + "/0"
+      fs.delete(new Path(modelHDFSPath), true)
+      fs.copyFromLocalFile(new Path(tempModelLocalPath),
+        new Path(modelHDFSPath))
+      // delete local model
+      FileUtils.deleteDirectory(new File(tempModelLocalPath))
+      f
+    })
+    val modelHDFSPath = SQLPythonFunc.getAlgModelPath(path) + "/0"
+    df.sparkSession.read.json(modelHDFSPath + "/attributes.json")
+      .write.mode(SaveMode.Overwrite).parquet(getDataPath(path))
   }
 
   override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = {
+    throw new RuntimeException("predict method is not supported!")
   }
 
   override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = {
