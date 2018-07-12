d04e2fa00b23fe462d58c1d3eee56f7e9870f93d
WilliamZhu <allwefantasy@gmail.com>
2018-07-12 11:53:35 +0800

    https://github.com/allwefantasy/streamingpro/issues/347

---
+++ streamingpro-mlsql/src/test/scala/streaming/core/code/PythonCode.scala
@@ -0,0 +1,98 @@
+package streaming.core.code
+
+/**
+  * Created by allwefantasy on 12/7/2018.
+  */
+object PythonCode {
+  val pythonTrainCode =
+    """
+      |import mlsql_model
+      |import mlsql
+      |from sklearn.naive_bayes import MultinomialNB
+      |
+      |clf = MultinomialNB()
+      |
+      |mlsql.sklearn_configure_params(clf)
+      |
+      |
+      |def train(X, y, label_size):
+      |    clf.partial_fit(X, y, classes=range(label_size))
+      |
+      |
+      |mlsql.sklearn_batch_data(train)
+      |
+      |X_test, y_test = mlsql.get_validate_data()
+      |print("cool------")
+      |if len(X_test) > 0:
+      |    testset_score = clf.score(X_test, y_test)
+      |    print("mlsql_validation_score:%f" % testset_score)
+      |
+      |mlsql_model.sk_save_model(clf)
+      |
+    """.stripMargin
+
+  val pythonCodeEnableLocal =
+    """
+      |import mlsql_model
+      |import mlsql
+      |import os
+      |import json
+      |from pyspark.ml.linalg import Vectors
+      |from sklearn.naive_bayes import MultinomialNB
+      |
+      |clf = MultinomialNB()
+      |
+      |mlsql.sklearn_configure_params(clf)
+      |tempDataLocalPath = mlsql.internal_system_param["tempDataLocalPath"]
+      |
+      |print(tempDataLocalPath)
+      |files = [file for file in os.listdir(tempDataLocalPath) if file.endswith(".json")]
+      |res = []
+      |res_label = []
+      |for file in files:
+      |    with open(tempDataLocalPath + "/" + file) as f:
+      |        for line in f.readlines():
+      |            obj = json.loads(line)
+      |            f_size = obj["features"]["size"]
+      |            f_indices = obj["features"]["indices"]
+      |            f_values = obj["features"]["values"]
+      |            res.append(Vectors.sparse(f_size, f_indices, f_values).toArray())
+      |            res_label.append(obj["label"])
+      |
+      |
+      |def train(X, y, label_size):
+      |    clf.partial_fit(X, y, classes=range(label_size))
+      |
+      |
+      |train(res,res_label,2)
+      |
+      |X_test, y_test = mlsql.get_validate_data()
+      |print("cool------")
+      |if len(X_test) > 0:
+      |    testset_score = clf.score(X_test, y_test)
+      |    print("mlsql_validation_score:%f" % testset_score)
+      |
+      |mlsql_model.sk_save_model(clf)
+      |
+    """.stripMargin
+
+  val pythonPredictCode =
+    """
+      |from pyspark.ml.linalg import VectorUDT, Vectors
+      |import pickle
+      |import python_fun
+      |
+      |
+      |def predict(index, s):
+      |    items = [i for i in s]
+      |    feature = VectorUDT().deserialize(pickle.loads(items[0]))
+      |    print(pickle.loads(items[1])[0])
+      |    model = pickle.load(open(pickle.loads(items[1])[0]+"/model.pickle"))
+      |    y = model.predict([feature.toArray()])
+      |    print("------".format)
+      |    return [VectorUDT().serialize(Vectors.dense(y))]
+      |
+      |
+      |python_fun.udf(predict)
+      | """.stripMargin
+}
