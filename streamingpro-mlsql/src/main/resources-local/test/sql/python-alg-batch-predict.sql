af9c1d650ae731a90013dd0f676fd8cdbbf14644
WilliamZhu <allwefantasy@gmail.com>
2018-08-25 17:34:33 +0800

    添加新的内置变量${HOME}

--- streamingpro-mlsql/src/main/resources-local/test/sql/python-alg-batch-predict.sql
+++ streamingpro-mlsql/src/main/resources-local/test/sql/python-alg-batch-predict.sql
@@ -8,7 +8,7 @@ and `kafkaParam.bootstrap.servers`="127.0.0.1:9092"
 and `kafkaParam.topic`="test"
 and `kafkaParam.group_id`="g_test-1"
 
-and  `fitParam.modelPath`="${modelPath}"
+and  `fitParam.modelPath`="${HOME}/${modelPath}"
 
 and `systemParam.pythonPath`="python"
 and `systemParam.pythonVer`="2.7"
