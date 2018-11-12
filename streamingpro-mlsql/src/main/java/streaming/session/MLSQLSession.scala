98628da4faf41d65be6f08a2b1b01fad49515990
WilliamZhu <allwefantasy@gmail.com>
2018-11-12 11:40:11 +0800

    add user logout api

--- streamingpro-mlsql/src/main/java/streaming/session/MLSQLSession.scala
+++ streamingpro-mlsql/src/main/java/streaming/session/MLSQLSession.scala
@@ -29,24 +29,8 @@ class MLSQLSession(username: String,
 
   private[this] val activeOperationSet = new MHSet[String]()
 
-  private[this] val sessionUGI: UserGroupInformation = {
-    val currentUser = UserGroupInformation.getCurrentUser
-    if (withImpersonation) {
-      if (UserGroupInformation.isSecurityEnabled) {
-        if (sessionConf.contains(MLSQLSparkConst.PRINCIPAL) && sessionConf.contains(MLSQLSparkConst.KEYTAB)) {
-          // If principal and keytab are configured, do re-login in case of token expiry.
-          // Do not check keytab file existing as spark-submit has it done
-          currentUser.reloginFromKeytab()
-        }
-        UserGroupInformation.createProxyUser(username, currentUser)
-      } else {
-        UserGroupInformation.createRemoteUser(username)
-      }
-    } else {
-      currentUser
-    }
-  }
-  private[this] lazy val sparkSessionWithUGI = new SparkSessionWithUGI(sessionUGI, sessionConf)
+
+  private[this] lazy val _mlsqlSparkSession = new MLSQLSparkSession(username, sessionConf)
 
   private[this] def acquire(userAccess: Boolean): Unit = {
     if (userAccess) {
@@ -65,12 +49,12 @@ class MLSQLSession(username: String,
     }
   }
 
-  def sparkSession: SparkSession = this.sparkSessionWithUGI.sparkSession
+  def sparkSession: SparkSession = this._mlsqlSparkSession.sparkSession
 
-  def ugi: UserGroupInformation = this.sessionUGI
+  def mlsqlSparkSession: MLSQLSparkSession = this._mlsqlSparkSession
 
   def open(sessionConf: Map[String, String]): Unit = {
-    sparkSessionWithUGI.init(sessionConf)
+    mlsqlSparkSession.init(sessionConf)
     lastAccessTime = System.currentTimeMillis
     lastIdleTime = lastAccessTime
   }
@@ -79,59 +63,17 @@ class MLSQLSession(username: String,
     acquire(true)
     try {
       // Iterate through the opHandles and close their operations
-      activeOperationSet.foreach { op => opManager.closeOp(op) }
       activeOperationSet.clear()
     } finally {
       release(true)
-      try {
-        FileSystem.closeAllForUGI(sessionUGI)
-      } catch {
-        case ioe: IOException =>
-          throw new MLSQLException("Could not clean up file-system handles for UGI: "
-            + sessionUGI, ioe)
-      }
     }
   }
 
-  def cancelOp(opName: String): Unit = {
-    acquire(true)
-    try {
-      opManager.cancelOp(opName)
-      activeOperationSet.remove(opName)
-    } finally {
-      release(true)
-    }
-  }
 
   def visit(): MLSQLSession = {
     acquire(true)
     release(true)
     this
-  }
-
-  def execute(f: () => Unit, opId: String, desc: String, operationTimeout: Int): MLSQLOperation = {
-    acquire(true)
-    val operation = opManager.createOp(this, f, opId, desc, operationTimeout)
-    try {
-      operation.run()
-      activeOperationSet.add(operation.getOpId)
-    } catch {
-      case e: MLSQLException =>
-        operation.close()
-        throw e
-    } finally {
-      release(true)
-    }
-    operation
-  }
-
-  def sql(sql: String, opId: String, desc: String, operationTimeout: Int) = {
-    var result: DataFrame = null
-    val operation = execute(() => {
-      result = sparkSession.sql(sql)
-    }, opId, desc, operationTimeout)
-    operation.getResult.get()
-    result
   }
 
 
