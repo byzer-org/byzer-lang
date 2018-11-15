4a7dd8a4a1d5fdd9a4c3cd63dab42c5fcfccc766
WilliamZhu <allwefantasy@gmail.com>
2018-11-15 22:33:05 +0800

    add download release python script

--- dev/mlsqltestssupport/aliyun/download_release.py
+++ dev/mlsqltestssupport/aliyun/download_release.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-
 import os
 import mlsqltestssupport.aliyun.config as config
-
 
 if not os.environ['MLSQL_RELEASE_TAR']:
     raise ValueError('MLSQL_RELEASE_TAR should be configured')
@@ -11,6 +9,6 @@ fileName = os.environ['MLSQL_RELEASE_TAR']
 
 bucket = config.ossClient()
 
-bucket.put_object_from_file(fileName.split("/")[-1], fileName)
+bucket.get_object_to_file(fileName.split("/")[-1], fileName)
 
-print("success uploaded")
+print("download successful")
