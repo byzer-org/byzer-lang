5f1e77bf08cf416a55a0200c5229475c0d0a4df3
WilliamZhu <allwefantasy@gmail.com>
2018-11-15 22:37:30 +0800

    change download to internal

--- dev/mlsqltestssupport/aliyun/download_release.py
+++ dev/mlsqltestssupport/aliyun/download_release.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import oss2
 import mlsqltestssupport.aliyun.config as config
 
 if not os.environ['MLSQL_RELEASE_TAR']:
@@ -7,7 +8,12 @@ if not os.environ['MLSQL_RELEASE_TAR']:
 
 fileName = os.environ['MLSQL_RELEASE_TAR']
 
-bucket = config.ossClient()
+if not os.environ['AK']:
+    raise ValueError('AK and AKS should be configured')
+
+auth = oss2.Auth(os.environ['AK'], os.environ['AKS'])
+
+bucket = oss2.Bucket(auth, "oss-cn-hangzhou-internal.aliyuncs.com", config.bucket_name)
 
 bucket.get_object_to_file(fileName.split("/")[-1], fileName)
 
