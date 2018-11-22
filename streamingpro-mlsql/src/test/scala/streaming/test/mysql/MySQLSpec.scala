594ac513be01506d4c84ca9e1c558bbbbd7f9231
WilliamZhu <allwefantasy@gmail.com>
2018-11-22 10:36:08 +0800

    fix #685

--- streamingpro-mlsql/src/test/scala/streaming/test/mysql/MySQLSpec.scala
+++ streamingpro-mlsql/src/test/scala/streaming/test/mysql/MySQLSpec.scala
@@ -9,7 +9,7 @@ import streaming.dsl.ScriptSQLExec
   * Created by allwefantasy on 12/9/2018.
   */
 class MySQLSpec extends BasicSparkOperation with SpecFunctions with BasicMLSQLConfig {
-  "save mysql with update" should "work fine" taggedAs (NotToRunTag) in {
+  "save mysql with update" should "work fine"  in {
 
     withBatchContext(setupBatchContext(batchParams, "classpath:///test/empty.json")) { runtime: SparkRuntime =>
       //执行sql
@@ -17,7 +17,7 @@ class MySQLSpec extends BasicSparkOperation with SpecFunctions with BasicMLSQLCo
 
       //注册表连接
       var sq = createSSEL
-      ScriptSQLExec.parse("connect jdbc where driver=\"com.mysql.jdbc.Driver\"\nand url=\"jdbc:mysql://127.0.0.1:3306/wow?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false\"\nand driver=\"com.mysql.jdbc.Driver\"\nand user=\"root\"\nand password=\"csdn.net\"\nas tableau;", sq)
+      ScriptSQLExec.parse("connect jdbc where driver=\"com.mysql.jdbc.Driver\"\nand url=\"jdbc:mysql://127.0.0.1:3306/wow?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false\"\nand driver=\"com.mysql.jdbc.Driver\"\nand user=\"root\"\nand password=\"mlsql\"\nas tableau;", sq)
 
       sq = createSSEL
       ScriptSQLExec.parse("select \"a\" as a,\"b\" as b\n,\"c\" as c\nas tod_boss_dashboard_sheet_1;", sq)
@@ -64,7 +64,7 @@ class MySQLSpec extends BasicSparkOperation with SpecFunctions with BasicMLSQLCo
     }
   }
 
-  "save mysql with default" should "work fine" taggedAs (NotToRunTag) in {
+  "save mysql with default" should "work fine" in {
 
     withBatchContext(setupBatchContext(batchParams, "classpath:///test/empty.json")) { runtime: SparkRuntime =>
       //执行sql
