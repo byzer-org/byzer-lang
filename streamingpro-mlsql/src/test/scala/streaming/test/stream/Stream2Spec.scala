f5c94ec884f5ca971d5113b955a376a412268c48
WilliamZhu <allwefantasy@gmail.com>
2018-12-20 11:09:12 +0800

    Using docker to make unitest more easy ;kafka example

--- streamingpro-mlsql/src/test/scala/streaming/test/stream/Stream2Spec.scala
+++ streamingpro-mlsql/src/test/scala/streaming/test/stream/Stream2Spec.scala
@@ -18,7 +18,6 @@
 
 package streaming.test.stream
 
-import org.apache.spark.SparkCoreVersion
 import org.apache.spark.streaming.BasicSparkOperation
 import org.scalatest.BeforeAndAfterAll
 import streaming.common.shell.ShellCommand
@@ -27,73 +26,67 @@ import streaming.core.{BasicMLSQLConfig, SpecFunctions}
 import streaming.dsl.ScriptSQLExec
 
 class Stream2Spec extends BasicSparkOperation with SpecFunctions with BasicMLSQLConfig with BeforeAndAfterAll {
-  val KAFKA_1_0_HOME = System.getenv("KAFKA_1_0_HOME")
   val topic_name = "test_cool"
   "kafka" should "work fine on spark 2.3.x" in {
     withBatchContext(setupBatchContext(batchParams, "classpath:///test/empty.json")) { runtime: SparkRuntime =>
       //执行sql
       implicit val spark = runtime.sparkSession
-      // we suppose that if KAFKA_HOME is configured ,then there must be a kafka server exists
-      if (KAFKA_1_0_HOME != null) {
+      ShellCommand.execCmd("rm -rf /tmp/cpl3")
 
-        ShellCommand.execCmd("rm -rf /tmp/cpl3")
-
-        new Thread(new Runnable {
-          override def run(): Unit = {
-            var count = 20
-            while (count > 0) {
-              val ssel = createSSEL
-              try {
-                ScriptSQLExec.parse(
-                  """
-                    |select "a" as value as tmp_table1;
-                    |save append tmp_table1
-                    |as kafka.`test_cool` where kafka.bootstrap.servers="127.0.0.1:9092";
-                  """.stripMargin, ssel)
-              } catch {
-                case e: Exception =>
-                  print(e.getMessage)
-              }
-              Thread.sleep(1000)
-              count -= 1
+      new Thread(new Runnable {
+        override def run(): Unit = {
+          var count = 20
+          while (count > 0) {
+            val ssel = createSSEL
+            try {
+              ScriptSQLExec.parse(
+                """
+                  |select "a" as value as tmp_table1;
+                  |save append tmp_table1
+                  |as kafka.`test_cool` where kafka.bootstrap.servers="127.0.0.1:9092";
+                """.stripMargin, ssel)
+            } catch {
+              case e: Exception =>
+                print(e.getMessage)
             }
-
+            Thread.sleep(1000)
+            count -= 1
           }
-        }).start()
 
-        val ssel = createSSEL
-        ScriptSQLExec.parse(
-          """
-            |set streamName="streamExample";
-            |load kafka.`test_cool` options kafka.bootstrap.servers="127.0.0.1:9092"
-            |as table1;
-            |
-            |save append table1
-            |as console.``
-            |options mode="Append"
-            |and duration="2"
-            |and checkpointLocation="/tmp/cpl3";
-          """.stripMargin, ssel)
-        Thread.sleep(1000 * 30)
-        assume(spark.streams.active.size>0)
-        spark.streams.active.foreach(f => f.stop())
-      }
+        }
+      }).start()
+
+      val ssel = createSSEL
+      ScriptSQLExec.parse(
+        """
+          |set streamName="streamExample";
+          |load kafka.`test_cool` options kafka.bootstrap.servers="127.0.0.1:9092"
+          |as table1;
+          |
+          |save append table1
+          |as console.``
+          |options mode="Append"
+          |and duration="2"
+          |and checkpointLocation="/tmp/cpl3";
+        """.stripMargin, ssel)
+      Thread.sleep(1000 * 30)
+      assume(spark.streams.active.size > 0)
+      spark.streams.active.foreach(f => f.stop())
 
     }
   }
 
-  val kafkaServer = new KafkaServer(Option(KAFKA_1_0_HOME), Seq(topic_name))
+  val kafkaServer = new streaming.test.servers.KafkaServer("1.1.1")
 
   override protected def beforeAll(): Unit = {
-    if (KAFKA_1_0_HOME != null) {
-      kafkaServer.startServer
-    }
+    kafkaServer.startServer
+    kafkaServer.exec("kafka",
+      s"""
+         |kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic ${topic_name}
+      """.stripMargin)
   }
 
   override protected def afterAll(): Unit = {
-    if (KAFKA_1_0_HOME != null) {
-      kafkaServer.shutdownServer
-    }
-
+    kafkaServer.stopServer
   }
 }
