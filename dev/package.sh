37a8b7f7a0697a1e45c51072debb037ef3af532c
CodeOverflow <this.is.lyd@gmail.com>
2018-12-21 11:55:17 +0800

    拼写错误
    MLSQL_SPARK_VERSIOIN ->MLSQL_SPARK_VERSION

--- dev/package.sh
+++ dev/package.sh
@@ -6,7 +6,7 @@ usage: package
 run package command based on different spark version.
 Inputs are specified with the following environment variables:
 
-MLSQL_SPARK_VERSIOIN - the spark version, 2.2/2.3/2.4
+MLSQL_SPARK_VERSION - the spark version, 2.2/2.3/2.4
 DRY_RUN true|false
 DISTRIBUTION true|false
 EOF
@@ -20,7 +20,7 @@ if [[ $@ == *"help"* ]]; then
   exit_with_usage
 fi
 
-for env in MLSQL_SPARK_VERSIOIN DRY_RUN DISTRIBUTION; do
+for env in MLSQL_SPARK_VERSION DRY_RUN DISTRIBUTION; do
   if [[ -z "${!env}" ]]; then
     echo "===$env must be set to run this script==="
     echo "===Please run ./dev/package.sh help to get how to use.==="
@@ -40,13 +40,13 @@ fi
 
 BASE_PROFILES="-Pscala-2.11 -Ponline -Phive-thrift-server -Pcarbondata  -Pcrawler"
 
-if [[ "$MLSQL_SPARK_VERSIOIN" > "2.2" ]]; then
+if [[ "$MLSQL_SPARK_VERSION" > "2.2" ]]; then
   BASE_PROFILES="$BASE_PROFILES -Pdsl -Pxgboost"
 else
   BASE_PROFILES="$BASE_PROFILES -Pdsl-legacy"
 fi
 
-BASE_PROFILES="$BASE_PROFILES -Pspark-$MLSQL_SPARK_VERSIOIN.0 -Pstreamingpro-spark-$MLSQL_SPARK_VERSIOIN.0-adaptor"
+BASE_PROFILES="$BASE_PROFILES -Pspark-$MLSQL_SPARK_VERSION.0 -Pstreamingpro-spark-$MLSQL_SPARK_VERSION.0-adaptor"
 
 if [[ ${DISTRIBUTION} == "true" ]];then
 BASE_PROFILES="$BASE_PROFILES -Passembly"
