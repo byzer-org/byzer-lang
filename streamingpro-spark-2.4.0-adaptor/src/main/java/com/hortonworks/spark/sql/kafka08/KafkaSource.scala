d7c058161d49277305efeae129e7b7b8f3b442ca
WilliamZhu <allwefantasy@gmail.com>
2019-01-03 17:21:47 +0800

    Kafka 0.8/0.9 bug in spark 2.4.x

--- streamingpro-spark-2.4.0-adaptor/src/main/java/com/hortonworks/spark/sql/kafka08/KafkaSource.scala
+++ streamingpro-spark-2.4.0-adaptor/src/main/java/com/hortonworks/spark/sql/kafka08/KafkaSource.scala
@@ -35,31 +35,31 @@
 
 package com.hortonworks.spark.sql.kafka08
 
-import scala.annotation.tailrec
-
+import com.hortonworks.spark.sql.kafka08.KafkaSource._
+import com.hortonworks.spark.sql.kafka08.util.Logging
 import kafka.common.TopicAndPartition
 import kafka.message.MessageAndMetadata
 import kafka.serializer.DefaultDecoder
 import org.apache.spark.SparkException
 import org.apache.spark.sql._
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.execution.streaming._
 import org.apache.spark.sql.types._
-import org.apache.spark.streaming.kafka.{Broker, KafkaCluster, KafkaUtils, OffsetRange}
 import org.apache.spark.streaming.kafka.KafkaCluster.LeaderOffset
-import com.hortonworks.spark.sql.kafka08.KafkaSource._
+import org.apache.spark.streaming.kafka.{Broker, KafkaCluster, KafkaUtils, OffsetRange}
 
-import com.hortonworks.spark.sql.kafka08.util.Logging
+import scala.annotation.tailrec
 
 /**
- * A [[Source]] that uses Kafka's SimpleConsumer API to reads data from Kafka.
- */
+  * A [[Source]] that uses Kafka's SimpleConsumer API to reads data from Kafka.
+  */
 case class KafkaSource(
-    sqlContext: SQLContext,
-    topics: Set[String],
-    kafkaParams: Map[String, String],
-    sourceOptions: Map[String, String],
-    metadataPath: String,
-    startFromSmallestOffset: Boolean)
+                        sqlContext: SQLContext,
+                        topics: Set[String],
+                        kafkaParams: Map[String, String],
+                        sourceOptions: Map[String, String],
+                        metadataPath: String,
+                        startFromSmallestOffset: Boolean)
   extends Source with Logging {
 
   private val sc = sqlContext.sparkContext
@@ -70,7 +70,7 @@ case class KafkaSource(
     sourceOptions.getOrElse("fetchOffset.numRetries", "3").toInt
 
   private lazy val initialPartitionOffsets = {
-    val metadataLog = Kafka08HDFSMetadataLog.create(sqlContext, metadataPath,VERSION)
+    val metadataLog = Kafka08HDFSMetadataLog.create(sqlContext, metadataPath, VERSION)
     metadataLog.get(0).getOrElse {
       val offsets = for {
         leaderOffsets <- (if (startFromSmallestOffset) {
@@ -101,10 +101,10 @@ case class KafkaSource(
   }
 
   /**
-   * Returns the data that is between the offsets
-   * [`start.get.partitionToOffsets`, `end.partitionToOffsets`), i.e. end.partitionToOffsets is
-   * exclusive.
-   */
+    * Returns the data that is between the offsets
+    * [`start.get.partitionToOffsets`, `end.partitionToOffsets`), i.e. end.partitionToOffsets is
+    * exclusive.
+    */
   override def getBatch(start: Option[Offset], end: Offset): DataFrame = {
     // Make sure initialPartitionOffsets is initialized
     initialPartitionOffsets
@@ -131,26 +131,27 @@ case class KafkaSource(
       Row(mmd.key(), mmd.message(), mmd.topic, mmd.partition, mmd.offset)
     }
 
+    val enconder = RowEncoder.apply(schema).resolveAndBind()
     // Create a RDD that reads from Kafka and get the (key, value) pair as byte arrays.
     val rdd = KafkaUtils.createRDD[
       Array[Byte],
       Array[Byte],
       DefaultDecoder,
       DefaultDecoder,
-      Row](sc, kafkaParams, offsetRanges, leaders, messageHandler)
+      Row](sc, kafkaParams, offsetRanges, leaders, messageHandler).map(row => enconder.toRow(row))
 
     info("GetBatch generating RDD of offset range: " + offsetRanges.sortBy(_.topic).mkString(","))
-    sqlContext.createDataFrame(rdd, schema)
+    new SessionWrapper(sqlContext.sparkSession).internalCreateDataFrame(rdd, schema, isStreaming = true)
   }
 
   /** Stop this source and free any resources it has allocated. */
-  override def stop(): Unit = { }
+  override def stop(): Unit = {}
 
   override def toString(): String = s"KafkaSource for topics [${topics.mkString(",")}]"
 
   /**
-   * Fetch the latest offset of partitions.
-   */
+    * Fetch the latest offset of partitions.
+    */
   @tailrec
   private def fetchLatestOffsets(retries: Int): Map[TopicAndPartition, LeaderOffset] = {
     val offsets = kc.getLatestLeaderOffsets(topicPartitions)
@@ -172,6 +173,7 @@ case class KafkaSource(
 /** Companion object for the [[KafkaSource]]. */
 private[kafka08] object KafkaSource {
   private[kafka08] val VERSION = 8
+
   def kafkaSchema: StructType = StructType(Seq(
     StructField("key", BinaryType),
     StructField("value", BinaryType),
