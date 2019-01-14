4022456497f27f238544575665da5c4da8bde23e
WilliamZhu <allwefantasy@gmail.com>
2019-01-14 17:02:00 +0800

    add datasource repository support

--- streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLDataSourceExt.scala
+++ streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLDataSourceExt.scala
@@ -6,28 +6,42 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import streaming.core.datasource.DataSourceRepository
 import streaming.dsl.mmlib.SQLAlg
 import streaming.dsl.mmlib.algs.param.{BaseParams, WowParams}
+import streaming.log.{Logging, WowLog}
 
 /**
   * 2019-01-14 WilliamZhu(allwefantasy@gmail.com)
   */
-class SQLDataSourceExt(override val uid: String) extends SQLAlg with WowParams {
+class SQLDataSourceExt(override val uid: String) extends SQLAlg with WowParams with Logging with WowLog {
 
 
   override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
-    val repository = new DataSourceRepository()
+
     params.get(command.name).map { item =>
       set(command, item)
       item
     }.getOrElse {
       set(command, "list")
     }
+
+    params.get(repository.name).map { item =>
+      set(repository, item)
+      item
+    }
+
+    val dataSourceRepository = new DataSourceRepository($(repository))
+
     val spark = df.sparkSession
     import spark.implicits._
     $(command) match {
       case "list" =>
-        spark.read.json(spark.createDataset(repository.listCommand))
-      case "version" =>
-
+        spark.read.json(spark.createDataset(dataSourceRepository.listCommand))
+      case "add" =>
+        val Array(dsFormat, groupid, artifactId, version) = path.split("/")
+        val url = dataSourceRepository.addCommand(dsFormat, groupid, artifactId, version)
+        val logMsg = format(s"Datasource is loading jar from ${url}")
+        logInfo(logMsg)
+        spark.sparkContext.addJar(url)
+        Seq[Seq[String]](Seq(logMsg)).toDF("desc")
     }
   }
 
@@ -47,6 +61,8 @@ class SQLDataSourceExt(override val uid: String) extends SQLAlg with WowParams {
   final val command: Param[String] = new Param[String](this, "command", "list|version|add", isValid = (m: String) => {
     m == "list" || m == "version" || m == "add"
   })
+
+  final val repository: Param[String] = new Param[String](this, "repository", "repository url")
 
   def this() = this(BaseParams.randomUID())
 }
