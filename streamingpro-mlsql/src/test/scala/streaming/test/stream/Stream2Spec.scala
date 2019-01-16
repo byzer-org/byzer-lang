a0b770ae172118734c2af5e4a77ef29621fc5bb8
WilliamZhu <allwefantasy@gmail.com>
2019-01-16 13:08:36 +0800

    `driver-statement-0` is not effective in save statement when sink is mysql and this is will pick by v1.1.7.1

--- streamingpro-mlsql/src/test/scala/streaming/test/stream/Stream2Spec.scala
+++ streamingpro-mlsql/src/test/scala/streaming/test/stream/Stream2Spec.scala
@@ -27,7 +27,7 @@ import streaming.dsl.ScriptSQLExec
 
 class Stream2Spec extends BasicSparkOperation with SpecFunctions with BasicMLSQLConfig with BeforeAndAfterAll {
   val topic_name = "test_cool"
-  "kafka" should "work fine on spark 2.3.x" in {
+  "kafka" should "work fine on spark" in {
     withBatchContext(setupBatchContext(batchParams, "classpath:///test/empty.json")) { runtime: SparkRuntime =>
       //执行sql
       implicit val spark = runtime.sparkSession
@@ -76,7 +76,90 @@ class Stream2Spec extends BasicSparkOperation with SpecFunctions with BasicMLSQL
     }
   }
 
+  "mysql-sinke" should "work fine " in {
+    withBatchContext(setupBatchContext(batchParams, "classpath:///test/empty.json")) { runtime: SparkRuntime =>
+      //执行sql
+      implicit val spark = runtime.sparkSession
+      ShellCommand.execCmd("rm -rf /tmp/cpl3")
+
+      val connect_stat =
+        s"""
+           |set user="root";
+           |connect jdbc where
+           |url="jdbc:mysql://127.0.0.1:3306/wow?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false"
+           |and driver="com.mysql.jdbc.Driver"
+           |and user="$${user}"
+           |and password="${password}"
+           |as wow_proxy;
+    """.stripMargin
+
+      ScriptSQLExec.parse(connect_stat, createSSEL)
+
+      new Thread(new Runnable {
+        override def run(): Unit = {
+          var count = 20
+          while (count > 0) {
+            val ssel = createSSEL
+            try {
+              ScriptSQLExec.parse(
+                """
+                  |select "a" as value as tmp_table1;
+                  |save append tmp_table1
+                  |as kafka.`test_cool` where kafka.bootstrap.servers="127.0.0.1:9092";
+                """.stripMargin, ssel)
+            } catch {
+              case e: Exception =>
+                print(e.getMessage)
+            }
+            Thread.sleep(1000)
+            count -= 1
+          }
+
+        }
+      }).start()
+
+      val ssel = createSSEL
+      ScriptSQLExec.parse(
+        """
+          |set streamName="streamExample";
+          |load kafka.`test_cool` options kafka.bootstrap.servers="127.0.0.1:9092"
+          |as table1;
+          |
+          |select cast(value as string) as k, 1 as c from table1 as resultDataTable;
+          |
+          |save append resultDataTable
+          |as jdbc.`wow_proxy.test1`
+          |options mode="Append"
+          |and `driver-statement-0`='''
+          |     CREATE TABLE IF NOT EXISTS test1
+          |       (
+          |          id   int(11) NOT NULL AUTO_INCREMENT,
+          |          k    varchar(255) DEFAULT NULL,
+          |          c    int(11),
+          |          PRIMARY KEY (id)
+          |       ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          |'''
+          |and `statement-0`="insert into test1(k,c) values(?,?)"
+          |and duration="3"
+          |and checkpointLocation="/tmp/cpl3";
+        """.stripMargin, ssel)
+      Thread.sleep(1000 * 30)
+      assume(spark.streams.active.size > 0)
+      spark.streams.active.foreach(f => f.stop())
+      ScriptSQLExec.parse(
+        """
+          |load jdbc.`wow_proxy.test1` as output;
+        """.stripMargin, ssel)
+
+      val df = spark.sql("select * from output")
+      df.show()
+      assume(df.count() > 0)
+
+    }
+  }
+
   val kafkaServer = new streaming.test.servers.KafkaServer("1.1.1")
+  val mysqlServer = new streaming.test.servers.MySQLServer("5.7")
 
   override protected def beforeAll(): Unit = {
     kafkaServer.startServer
@@ -84,9 +167,12 @@ class Stream2Spec extends BasicSparkOperation with SpecFunctions with BasicMLSQL
       s"""
          |kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic ${topic_name}
       """.stripMargin)
+    mysqlServer.startServer
+    mysqlServer.exec("mysql", "exec mysql -uroot -pmlsql --protocol=tcp -e 'create database wow'")
   }
 
   override protected def afterAll(): Unit = {
     kafkaServer.stopServer
+    mysqlServer.stopServer
   }
 }
