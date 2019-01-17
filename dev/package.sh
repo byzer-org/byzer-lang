03c0c86fbb1a24f981fc5e8f933c66942217c04b
WilliamZhu <allwefantasy@gmail.com>
2019-01-17 10:20:42 +0800

    add deploy command in package

--- dev/package.sh
+++ dev/package.sh
@@ -51,20 +51,31 @@ BASE_PROFILES="$BASE_PROFILES -Pspark-$MLSQL_SPARK_VERSION.0 -Pstreamingpro-spar
 if [[ ${DISTRIBUTION} == "true" ]];then
 BASE_PROFILES="$BASE_PROFILES -Passembly"
 else
-BASE_PROFILES="$BASE_PROFILES -Pshade -pl streamingpro-mlsql -am"
+BASE_PROFILES="$BASE_PROFILES -pl streamingpro-mlsql -am"
 fi
 
 export MAVEN_OPTS="-Xmx6000m"
 
 SKIPTEST=""
 TESTPROFILE=""
-if [[ "$COMMAND" != "package" ]];then
-     TESTPROFILE="-Punit-test"
-     #COMMAND="surefire-report:report"
-else
-     SKIPTEST="-DskipTests"
+
+if [[ "${COMMAND}" == "package" ]];then
+  BASE_PROFILES="$BASE_PROFILES -Pshade"
 fi
 
+if [[ "${COMMAND}" == "package" || "${COMMAND}" == "deploy" ]];then
+   SKIPTEST="-DskipTests"
+fi
+
+
+if [[ "${COMMAND}" == "test" ]];then
+   TESTPROFILE="-Punit-test"
+fi
+
+if [[ "${COMMAND}" == "deploy" ]];then
+   BASE_PROFILES="$BASE_PROFILES -Prelease-sign-artifacts"
+   BASE_PROFILES="$BASE_PROFILES -Pdisable-java8-doclint"
+fi
 
 if [[ ${DRY_RUN} == "true" ]];then
 
@@ -73,6 +84,9 @@ mvn clean ${COMMAND}  ${SKIPTEST} ${BASE_PROFILES}  ${TESTPROFILE}
 EOF
 
 else
+cat << EOF
+mvn clean ${COMMAND}  ${SKIPTEST} ${BASE_PROFILES}  ${TESTPROFILE}
+EOF
 mvn clean ${COMMAND}  ${SKIPTEST} ${BASE_PROFILES} ${TESTPROFILE}
 fi
 
