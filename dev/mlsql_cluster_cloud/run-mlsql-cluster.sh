93b18f485bf023907d8a31388dd03caf15ca0067
WilliamZhu <allwefantasy@gmail.com>
2019-03-02 09:55:59 +0800

    increase spark.kryoserializer.buffer.max

--- dev/mlsql_cluster_cloud/run-mlsql-cluster.sh
+++ dev/mlsql_cluster_cloud/run-mlsql-cluster.sh
@@ -396,8 +396,8 @@ nohup ./bin/spark-submit --class streaming.core.StreamingApp \
         --master spark://${inter_ip}:7077 \
         --deploy-mode client ${EXECUTOR_CORES_CONFIG} \
         --name mlsql \
-        --conf "spark.kryoserializer.buffer=256k" \
-        --conf "spark.kryoserializer.buffer.max=1024m" \
+        --conf "spark.kryoserializer.buffer=4048k" \
+        --conf "spark.kryoserializer.buffer.max=4048m" \
         --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
         --conf "spark.scheduler.mode=FAIR" \
         --conf "spark.executor.extraClassPath=\${SPARK_HOME}/jars/*:.:\${SPARK_HOME}/conf/:/home/webuser/\${MAIN_JAR}" \
