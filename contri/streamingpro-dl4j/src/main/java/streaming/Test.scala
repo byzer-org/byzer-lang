2cfbf633c845b30a7cdf539a2758b9daa4ad2d54
WilliamZhu <allwefantasy@gmail.com>
2019-03-07 20:20:14 +0800

    support mysql/jdbc auto copy data to hdfs

--- contri/streamingpro-dl4j/src/main/java/streaming/Test.scala
+++ contri/streamingpro-dl4j/src/main/java/streaming/Test.scala
@@ -18,11 +18,13 @@
 
 package streaming
 
+import java.util.Date
+
 /**
   * Created by allwefantasy on 27/5/2018.
   */
 object Test {
   def main(args: Array[String]): Unit = {
-
+    print(new Date(1546531200000l))
   }
 }
---
+++ streamingpro-mlsql/src/test/scala/streaming/test/Test.scala
@@ -0,0 +1,12 @@
+package streaming.test
+
+import java.util.Date
+
+/**
+  * 2019-03-05 WilliamZhu(allwefantasy@gmail.com)
+  */
+object Test {
+  def main(args: Array[String]): Unit = {
+    print(new Date(1546704000000l))
+  }
+}
