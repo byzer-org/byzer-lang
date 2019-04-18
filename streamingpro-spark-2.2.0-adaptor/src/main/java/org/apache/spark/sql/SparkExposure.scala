fb36f1560837f1c26bb2e554fdf6e3a95432966a
WilliamZhu <allwefantasy@gmail.com>
2019-04-18 17:25:09 +0800

    update

--- streamingpro-spark-2.2.0-adaptor/src/main/java/org/apache/spark/sql/SparkExposure.scala
+++ streamingpro-spark-2.2.0-adaptor/src/main/java/org/apache/spark/sql/SparkExposure.scala
@@ -7,6 +7,6 @@ import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
   */
 object SparkExposure {
   def cleanCache(spark: SparkSession, plan: LogicalPlan) = {
-    spark.sqlContext.sharedState.cacheManager.uncacheQuery(spark, plan, false, true)
+    spark.sqlContext.sharedState.cacheManager.uncacheQuery(spark, plan, true)
   }
 }
