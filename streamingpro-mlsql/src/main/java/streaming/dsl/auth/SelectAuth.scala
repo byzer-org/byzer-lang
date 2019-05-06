856e6121f6aacd778920c7a90dc9820a939f7b37
WilliamZhu <allwefantasy@gmail.com>
2019-05-06 10:37:59 +0800

    fix select auth

--- streamingpro-mlsql/src/main/java/streaming/dsl/auth/SelectAuth.scala
+++ streamingpro-mlsql/src/main/java/streaming/dsl/auth/SelectAuth.scala
@@ -75,9 +75,9 @@ class SelectAuth(authProcessListener: AuthProcessListener) extends MLSQLAuth wit
       }
     }
 
-    val exists = authProcessListener.withoutDBs.filter(m => tableName == m.table.get).size > 0
+    val exists = authProcessListener.withoutDBs.filter(m => cleanStr(tableName) == m.table.get).size > 0
     if (!exists) {
-      authProcessListener.addTable(MLSQLTable(None, Some(tableName), OperateType.SELECT, None, TableType.TEMP))
+      authProcessListener.addTable(MLSQLTable(None, Some(cleanStr(tableName)), OperateType.SELECT, None, TableType.TEMP))
     }
 
 
