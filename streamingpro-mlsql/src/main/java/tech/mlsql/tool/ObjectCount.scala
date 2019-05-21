c2513887f381957333d564ea4e63552e1b940883
WilliamZhu <allwefantasy@gmail.com>
2019-05-21 16:06:30 +0800

    update

---
+++ streamingpro-mlsql/src/main/java/tech/mlsql/tool/ObjectCount.scala
@@ -0,0 +1,17 @@
+package tech.mlsql.tool
+
+/**
+  * 2019-05-20 WilliamZhu(allwefantasy@gmail.com)
+  */
+object ObjectCount {
+  def main(args: Array[String]): Unit = {
+//    val vm = ac.attach(connectArgs)
+//    val allClasses = vm.allClasses
+//    val instanceCounts = vm.instanceCounts(allClasses)
+//    for (int i = 0; i < allClasses.size(); i++)
+//    {
+//      System.out.println(allClasses.get(i).name()
+//        + ": " + instanceCounts[i]);
+//    }
+  }
+}
