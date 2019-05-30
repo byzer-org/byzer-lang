77485b279526650eedd885ac7802b01e8efb3726
WilliamZhu <allwefantasy@gmail.com>
2019-05-30 14:36:09 +0800

    fix typo

--- dev/docker/start-local.sh
+++ dev/docker/start-local.sh
@@ -17,7 +17,7 @@ MAIN_JAR=$(ls ${MLSQL_HOME}/libs|grep 'streamingpro-mlsql')
 export DRIVER_MEMORY=${DRIVER_MEMORY:-2g}
 $SPARK_HOME/bin/spark-submit --class streaming.core.StreamingApp \
         --driver-memory ${DRIVER_MEMORY} \
-        --jars ${JARS} \                         
+        --jars ${JARS} \
         --master local[*] \
         --name mlsql \
         --conf "spark.driver.extraJavaOptions"="-DREALTIME_LOG_HOME=/tmp/__mlsql__/logs" \
