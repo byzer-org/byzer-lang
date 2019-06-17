819d769b119704fcc7881dd3e6de7c0654b704c3
WilliamZhu <allwefantasy@gmail.com>
2019-06-17 17:12:29 +0800

    udpate

--- streamingpro-mlsql/src/main/java/tech/mlsql/datasource/impl/MLSQLBinlog.scala
+++ streamingpro-mlsql/src/main/java/tech/mlsql/datasource/impl/MLSQLBinlog.scala
@@ -2,6 +2,7 @@ package tech.mlsql.datasource.impl
 
 import org.apache.spark.sql.{DataFrame, DataFrameReader}
 import streaming.core.datasource.{DataSourceConfig, MLSQLBaseStreamSource}
+import streaming.dsl.ScriptSQLExec
 import streaming.dsl.mmlib.algs.param.{BaseParams, WowParams}
 
 /**
@@ -11,8 +12,15 @@ class MLSQLBinlog(override val uid: String) extends MLSQLBaseStreamSource with W
   def this() = this(BaseParams.randomUID())
 
   override def load(reader: DataFrameReader, config: DataSourceConfig): DataFrame = {
+    var parameters = config.config
+    if (config.config.contains("metadataPath")) {
+      val context = ScriptSQLExec.contextGetOrForTest()
+      val owner = config.config.get("owner").getOrElse(context.owner)
+      val path = resourceRealPath(context.execListener, Option(owner), config.config("metadataPath"))
+      parameters = parameters ++ Map("metadataPath" -> path)
+    }
     val streamReader = config.df.get.sparkSession.readStream
-    streamReader.options(rewriteConfig(config.config)).format(fullFormat).load()
+    streamReader.options(rewriteConfig(parameters)).format(fullFormat).load()
   }
 
   override def fullFormat: String = "org.apache.spark.sql.delta.sources.MLSQLBinLogDataSource"
