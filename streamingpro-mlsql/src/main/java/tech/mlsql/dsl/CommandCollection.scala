323cf0105646209227f7c8c2b88db92910843b94
WilliamZhu <allwefantasy@gmail.com>
2020-01-07 16:16:44 +0800

    add !desc tableName json command

--- streamingpro-mlsql/src/main/java/tech/mlsql/dsl/CommandCollection.scala
+++ streamingpro-mlsql/src/main/java/tech/mlsql/dsl/CommandCollection.scala
@@ -6,8 +6,8 @@ import streaming.dsl.{ScriptSQLExec, ScriptSQLExecListener}
 import scala.collection.JavaConverters._
 
 /**
-  * 2019-04-11 WilliamZhu(allwefantasy@gmail.com)
-  */
+ * 2019-04-11 WilliamZhu(allwefantasy@gmail.com)
+ */
 object CommandCollection {
 
   private val commandMapping = new java.util.concurrent.ConcurrentHashMap[String, String]()
@@ -26,7 +26,7 @@ object CommandCollection {
     commandMapping.asScala.foreach { k =>
       context.addEnv(k._1, s"""run command as ${k._2}.`` where parameters='''{:all}'''""")
     }
-    context.addEnv("desc", """run command as ShowTableExt.`{}`""")
+    context.addEnv("desc", """run command as ShowTableExt.`` where parameters='''{:all}''' """)
     context.addEnv("kill", """run command as Kill.`{}`""")
     context.addEnv("jdbc", """ run command as JDBC.`{}` where `driver-statement-0`='''{}''' """)
 
@@ -40,25 +40,25 @@ object CommandCollection {
     context.addEnv("createPythonEnvFromFile", """ run command as PythonEnvExt.`{}` where condaYamlFilePath="${HOME}/{}" and command="create"  """)
     context.addEnv("removePythonEnvFromFile", """ run command as PythonEnvExt.`{}` where condaYamlFilePath="${HOME}/{}" and command="remove" """)
 
-    context.addEnv("resource",""" run command as EngineResource.`` where action="{0}" and cpus="{1}" """)
+    context.addEnv("resource", """ run command as EngineResource.`` where action="{0}" and cpus="{1}" """)
 
-    context.addEnv("model",""" run command as ModelCommand.`{1}` where action="{0}" """)
+    context.addEnv("model", """ run command as ModelCommand.`{1}` where action="{0}" """)
 
-    context.addEnv("hdfs",""" run command as HDFSCommand.`` where parameters='''{:all}''' """)
-    context.addEnv("fs",""" run command as HDFSCommand.`` where parameters='''{:all}''' """)
+    context.addEnv("hdfs", """ run command as HDFSCommand.`` where parameters='''{:all}''' """)
+    context.addEnv("fs", """ run command as HDFSCommand.`` where parameters='''{:all}''' """)
 
-    context.addEnv("split",""" run {0} as RateSampler.`` where labelCol="{2}" and sampleRate="{4}" as {6} """)
+    context.addEnv("split", """ run {0} as RateSampler.`` where labelCol="{2}" and sampleRate="{4}" as {6} """)
 
-    context.addEnv("saveUploadFileToHome",""" run command as DownloadExt.`` where from="{}" and to="{}" """)
+    context.addEnv("saveUploadFileToHome", """ run command as DownloadExt.`` where from="{}" and to="{}" """)
 
-    context.addEnv("withWartermark",""" register WaterMarkInPlace.`` where inputTable="{}" and eventTimeCol="{}" and delayThreshold="{}" """)
+    context.addEnv("withWartermark", """ register WaterMarkInPlace.`` where inputTable="{}" and eventTimeCol="{}" and delayThreshold="{}" """)
 
-    context.addEnv("delta","""run command as DeltaCommandWrapper.`` where parameters='''{:all}'''""")
-    context.addEnv("scheduler","""run command as SchedulerCommand.`` where parameters='''{:all}'''""")
+    context.addEnv("delta", """run command as DeltaCommandWrapper.`` where parameters='''{:all}'''""")
+    context.addEnv("scheduler", """run command as SchedulerCommand.`` where parameters='''{:all}'''""")
 
-    context.addEnv("python","""run command as PythonCommand.`` where parameters='''{:all}'''""")
-    context.addEnv("ray","""run command as Ray.`` where parameters='''{:all}'''""")
-    context.addEnv("plugin","""run command as PluginCommand.`` where parameters='''{:all}'''""")
+    context.addEnv("python", """run command as PythonCommand.`` where parameters='''{:all}'''""")
+    context.addEnv("ray", """run command as Ray.`` where parameters='''{:all}'''""")
+    context.addEnv("plugin", """run command as PluginCommand.`` where parameters='''{:all}'''""")
 
     context.addEnv("kafkaTool",
       """ run command as KafkaCommand.`kafka` where
