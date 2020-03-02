fa9d1b1a9a158c6ba3acaa975cefee44ae823911
WilliamZhu <allwefantasy@gmail.com>
2020-03-02 11:00:48 +0800

    update

--- streamingpro-core/src/main/java/tech/mlsql/log/RedirectStreamsToSocketServer.scala
+++ streamingpro-core/src/main/java/tech/mlsql/log/RedirectStreamsToSocketServer.scala
@@ -1,6 +1,6 @@
 package tech.mlsql.log
 
-import java.io.{DataOutputStream, IOException, InputStream}
+import java.io.{DataOutputStream, InputStream}
 import java.net.Socket
 
 import tech.mlsql.arrow.Utils
@@ -9,8 +9,8 @@ import tech.mlsql.arrow.python.runner.PythonConf
 import tech.mlsql.common.utils.lang.sc.ScalaMethodMacros
 
 /**
-  * 2019-08-21 WilliamZhu(allwefantasy@gmail.com)
-  */
+ * 2019-08-21 WilliamZhu(allwefantasy@gmail.com)
+ */
 class RedirectStreamsToSocketServer extends RedirectStreams {
   var _conf: Map[String, String] = null
 
@@ -37,33 +37,86 @@ class RedirectLogThread(
   extends Thread(name) {
 
   setDaemon(true)
+  WriteLog.init(conf)
 
   override def run() {
-    WriteLog.write(scala.io.Source.fromInputStream(in).getLines(), conf, propagateEof)
+    WriteLog.write(scala.io.Source.fromInputStream(in).getLines())
+  }
+}
+
+class WriteLogPool(size: Int, conf: Map[String, String]) {
+  val pool = new java.util.concurrent.ConcurrentLinkedQueue[WriteLog]()
+
+  def init(): Unit = {
+    (0 until size).foreach { item =>
+      pool.add(createObj())
+    }
+  }
+
+  def borrowObj() = {
+    pool.poll()
+  }
+
+  def returnObj(writeLog: WriteLog) = {
+    pool.offer(writeLog)
+  }
+
+  private def createObj() = {
+    new WriteLog(conf)
+  }
+
+}
+
+class WriteLog(conf: Map[String, String]) {
+
+  val host = conf("spark.mlsql.log.driver.host")
+  val port = conf("spark.mlsql.log.driver.port")
+  val token = conf("spark.mlsql.log.driver.token")
+  val owner = conf.getOrElse(ScalaMethodMacros.str(PythonConf.PY_EXECUTE_USER), "")
+  val groupId = conf.getOrElse("groupId", "")
+  val socket = new Socket(host, port.toInt)
+
+
+  def write(in: Iterator[String]) = {
+    val dOut = new DataOutputStream(socket.getOutputStream)
+    in.foreach { line =>
+      val client = new DriverLogClient()
+      client.sendRequest(dOut, SendLog(token, LogUtils.formatWithOwner(line, owner, groupId)))
+    }
+  }
+
+  def close = {
+    socket.close()
   }
 }
 
 object WriteLog {
-  def write(in: Iterator[String], conf: Map[String, String], propagateEof: Boolean = false) = {
-    scala.util.control.Exception.ignoring(classOf[IOException]) {
-      val host = conf("spark.mlsql.log.driver.host")
-      val port = conf("spark.mlsql.log.driver.port")
-      val token = conf("spark.mlsql.log.driver.token")
-      val owner = conf.getOrElse(ScalaMethodMacros.str(PythonConf.PY_EXECUTE_USER), "")
-      val groupId = conf.getOrElse("groupId", "")
-      val socket = new Socket(host, port.toInt)
 
-      Utils.tryWithSafeFinally {
-        val dOut = new DataOutputStream(socket.getOutputStream)
-        in.foreach { line =>
-          val client = new DriverLogClient()
-          client.sendRequest(dOut, SendLog(token, LogUtils.formatWithOwner(line, owner, groupId)))
-        }
+  var POOL: WriteLogPool = null
 
-      } {
-        if (propagateEof) {
-          socket.close()
-        }
+  def init(conf: Map[String, String]) = {
+    synchronized {
+      if (POOL == null) {
+        POOL = new WriteLogPool(30, conf)
+      }
+    }
+
+  }
+
+  def write(in: Iterator[String]) = {
+    if (POOL == null) {
+      throw new RuntimeException("WriteLog Pool is not init")
+    }
+    val obj = POOL.borrowObj()
+    Utils.tryWithSafeFinally {
+      if (obj == null) {
+        throw new RuntimeException("Cannot get connection for task writing log to driver")
+      }
+      obj.write(in)
+
+    } {
+      if (obj != null) {
+        POOL.returnObj(obj)
       }
     }
   }
--- streamingpro-mlsql/src/main/java/tech/mlsql/ets/tensorflow/DistributedTensorflow.scala
+++ streamingpro-mlsql/src/main/java/tech/mlsql/ets/tensorflow/DistributedTensorflow.scala
@@ -151,7 +151,7 @@ class DistributedTensorflow(override val uid: String) extends SQLAlg with SQLPyt
               val runner = new PythonProjectRunner(taskDirectory, Map())
               val res = runner.run(command, paramMap ++ logConf)
               processRef.set(runner.getPythonProcess.get)
-              WriteLog.write(res, logConf, false)
+              WriteLog.write(res)
             } catch {
               case e: Exception =>
                 if (flag.get() != "kill-python") {
@@ -245,7 +245,7 @@ class DistributedTensorflow(override val uid: String) extends SQLAlg with SQLPyt
         val runner = new PythonProjectRunner(taskDirectory, Map())
         val res = runner.run(command, paramMap ++ logConf)
         processRef.set(runner.getPythonProcess.get)
-        WriteLog.write(res, logConf, false)
+        WriteLog.write(res)
         logInfo(s"report worker ${tfContext.currentRole.taskIndex} success")
         tfContext.reportSuccess
       } catch {
