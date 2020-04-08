25080362b05e05c348ee151b1e4a8669b369441d
WilliamZhu <allwefantasy@gmail.com>
2020-04-08 17:32:55 +0800

    wow

--- streamingpro-core/src/main/java/org/apache/spark/MLSQLSparkUtils.scala
+++ streamingpro-core/src/main/java/org/apache/spark/MLSQLSparkUtils.scala
@@ -1,8 +1,5 @@
 package org.apache.spark
 
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.execution.command.ExplainCommand
-
 /**
  * 2019-08-16 WilliamZhu(allwefantasy@gmail.com)
  */
@@ -11,7 +8,4 @@ object MLSQLSparkUtils {
     SparkEnv.get.rpcEnv
   }
 
-  def createExplainCommand(lg: LogicalPlan, extended: Boolean) = {
-    ExplainCommand(lg, extended = extended)
-  }
 }
