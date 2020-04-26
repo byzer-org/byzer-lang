c7b8ea44628a2015489aa49d199b06a5c82c0078
WilliamZhu <allwefantasy@gmail.com>
2020-04-26 12:47:14 +0800

    support script store

--- streamingpro-core/src/main/java/tech/mlsql/dsl/includes/StoreIncludeSource.scala
+++ streamingpro-core/src/main/java/tech/mlsql/dsl/includes/StoreIncludeSource.scala
@@ -1,30 +1,53 @@
 package tech.mlsql.dsl.includes
 
-import org.apache.http.client.fluent.Request
+import java.nio.charset.Charset
+
+import org.apache.http.client.fluent.{Form, Request}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.mlsql.session.MLSQLException
 import streaming.dsl.IncludeSource
 import tech.mlsql.common.utils.log.Logging
-import tech.mlsql.common.utils.path.PathFun
+import tech.mlsql.common.utils.serder.json.JSONTool
+import tech.mlsql.runtime.PluginStoreItem
 
 /**
-  * 2019-05-12 WilliamZhu(allwefantasy@gmail.com)
-  */
+ * 2019-05-12 WilliamZhu(allwefantasy@gmail.com)
+ */
 class StoreIncludeSource extends IncludeSource with Logging {
-  override def fetchSource(sparkSession: SparkSession, path: String, options: Map[String, String]): String = {
 
-    val res = Request.Get(PathFun("http://repo.store.mlsql.tech").add(path).toPath)
-      .connectTimeout(60 * 1000)
-      .socketTimeout(10 * 60 * 1000)
-      .execute().returnContent().asString()
+  val PLUGIN_STORE_URL = "http://store.mlsql.tech/run"
 
-    if (res == null) {
-      throw new MLSQLException(
-        s"""
-           |${path} is not found
-         """.stripMargin)
+  def getPluginInfo(name: String) = {
+    val pluginListResponse = Request.Post(PLUGIN_STORE_URL).connectTimeout(60 * 1000)
+      .socketTimeout(60 * 60 * 1000).bodyForm(Form.form().
+      add("action", "getPlugin").
+      add("pluginName", name).
+      add("pluginType", "MLSQL_SCRIPT").
+      build(),
+      Charset.forName("utf-8")).execute().returnContent().asString(Charset.forName("utf-8"))
+    JSONTool.parseJson[List[PluginStoreItem]](pluginListResponse)
+  }
+
+  def getLatestPluginInfo(name: String) = {
+    val plugins = getPluginInfo(name)
+    plugins.sortBy(_.version).last
+  }
+
+  def getPluginNameAndVersion(name: String): (String, String) = {
+    if (name.contains(":")) {
+      name.split(":") match {
+        case Array(name, version) => (name, version)
+      }
+    } else {
+      (name, getLatestPluginInfo(name).version)
     }
-    res
+  }
+
+  override def fetchSource(sparkSession: SparkSession, path: String, options: Map[String, String]): String = {
+    val (name, version) = getPluginNameAndVersion(path)
+    val pluginInfoOpt = getPluginInfo(name).filter(item => item.version == version).headOption
+    require(pluginInfoOpt.isDefined, s"Plugin ${name}:${version} is not exists")
+    val params = JSONTool.parseJson[Map[String, String]](pluginInfoOpt.get.extraParams)
+    params("content")
   }
 
   override def skipPathPrefix: Boolean = true
