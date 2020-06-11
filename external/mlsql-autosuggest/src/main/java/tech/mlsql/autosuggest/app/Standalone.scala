d4c09c3b458b32f468342a9d88640aac0f9b30b8
WilliamZhu <allwefantasy@gmail.com>
2020-06-11 14:34:49 +0800

    wow

--- external/mlsql-autosuggest/src/main/java/tech/mlsql/autosuggest/app/Standalone.scala
+++ external/mlsql-autosuggest/src/main/java/tech/mlsql/autosuggest/app/Standalone.scala
@@ -5,6 +5,7 @@ import net.csdn.annotation.rest.At
 import net.csdn.bootstrap.Application
 import net.csdn.modules.http.RestRequest.Method
 import net.csdn.modules.http.{ApplicationController, ViewType}
+import org.apache.spark.sql.SparkSession
 import tech.mlsql.autosuggest.AutoSuggestContext
 import tech.mlsql.common.utils.serder.json.JSONTool
 import tech.mlsql.common.utils.shell.command.ParamsUtil
@@ -13,9 +14,15 @@ import tech.mlsql.common.utils.shell.command.ParamsUtil
  * 11/6/2020 WilliamZhu(allwefantasy@gmail.com)
  */
 object Standalone extends {
+  var sparkSession: SparkSession = null
+
   def main(args: Array[String]): Unit = {
     AutoSuggestContext.init
     val params = new ParamsUtil(args)
+    val enableMLSQL = params.getParam("enableMLSQL", "false").toBoolean
+    if (enableMLSQL) {
+      sparkSession = SparkSession.builder().appName("local").master("local[*]").getOrCreate()
+    }
     val applicationYamlName = params.getParam("config", "application.yml")
     ServiceFramwork.applicaionYamlName(applicationYamlName)
     ServiceFramwork.scanService.setLoader(classOf[Standalone])
@@ -34,7 +41,7 @@ class SuggestController extends ApplicationController {
     val columnNum = param("columnNum").toInt
     val isDebug = param("isDebug", "false").toBoolean
 
-    val context = new AutoSuggestContext(null,
+    val context = new AutoSuggestContext(Standalone.sparkSession,
       AutoSuggestController.mlsqlLexer,
       AutoSuggestController.sqlLexer)
     context.setDebugMode(isDebug)
