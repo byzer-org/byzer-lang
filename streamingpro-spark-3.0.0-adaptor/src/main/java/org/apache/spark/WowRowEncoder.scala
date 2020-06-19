c0ec656a79d3cec74bb6b145ecbe24aad2014f0b
WilliamZhu <allwefantasy@gmail.com>
2020-06-19 11:27:45 +0800

    add WowRowEncoder

--- streamingpro-spark-3.0.0-adaptor/src/main/java/org/apache/spark/WowRowEncoder.scala
+++ streamingpro-spark-3.0.0-adaptor/src/main/java/org/apache/spark/WowRowEncoder.scala
@@ -1,5 +1,8 @@
 package org.apache.spark
-
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.StructType
 /**
  * 19/6/2020 WilliamZhu(allwefantasy@gmail.com)
  */
