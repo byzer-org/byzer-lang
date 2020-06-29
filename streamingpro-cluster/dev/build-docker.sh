22863c3d2f4620c9d1520b53b3742506e9909f9a
WilliamZhu <allwefantasy@gmail.com>
2020-06-29 18:56:21 +0800

    upgrade to 1.7.0-SNAPSHOT

--- streamingpro-cluster/dev/build-docker.sh
+++ streamingpro-cluster/dev/build-docker.sh
@@ -8,7 +8,9 @@ export MLSQL_CLUSTER_VERSION=${MLSQL_CLUSTER_VERSION:-1.2.0-SNAPSHOT}
 mvn -DskipTests -Pcluster-shade -am -pl streamingpro-cluster clean package
 cd $SELF
 cd ..
-cp target/streamingpro-cluster-${MLSQL_CLUSTER_VERSION}.jar ./dev/mlsql-cluster-docker
+## streamingpro-cluster-2.4_2.11-1.6.0.jar
+export JAR_NAME=streamingpro-cluster-2.4_2.11-${MLSQL_CLUSTER_VERSION}.jar
+cp target/${JAR_NAME} ./dev/mlsql-cluster-docker
 
 cd $SELF
-docker build --build-arg MLSQL_CLUSTER_JAR=streamingpro-cluster-${MLSQL_CLUSTER_VERSION}.jar -t mlsql-cluster:${MLSQL_CLUSTER_VERSION} ./mlsql-cluster-docker
+docker build --build-arg MLSQL_CLUSTER_JAR=${JAR_NAME} -t mlsql-cluster:${MLSQL_CLUSTER_VERSION} ./mlsql-cluster-docker
