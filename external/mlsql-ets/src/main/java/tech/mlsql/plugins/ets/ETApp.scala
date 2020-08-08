bf109654c3022e6e364a4aee4fd2954b388636c9
WilliamZhu <allwefantasy@gmail.com>
2020-08-08 12:15:38 +0800

    update

--- external/mlsql-ets/src/main/java/tech/mlsql/plugins/ets/ETApp.scala
+++ external/mlsql-ets/src/main/java/tech/mlsql/plugins/ets/ETApp.scala
@@ -1,5 +1,7 @@
 package tech.mlsql.plugins.ets
 
+import tech.mlsql.dsl.CommandCollection
+import tech.mlsql.ets.register.ETRegister
 import tech.mlsql.version.VersionCompatibility
 
 /**
@@ -7,8 +9,8 @@ import tech.mlsql.version.VersionCompatibility
  */
 class ETApp  extends tech.mlsql.app.App with VersionCompatibility {
   override def run(args: Seq[String]): Unit = {
-//    ETRegister.register(ProfilerApp.MODULE_NAME, classOf[ProfilerCommand].getName)
-//    CommandCollection.refreshCommandMapping(Map(ProfilerApp.COMMAND_NAME -> ProfilerApp.MODULE_NAME))
+    ETRegister.register("Pivot", classOf[Pivot].getName)
+    //CommandCollection.refreshCommandMapping(Map(ProfilerApp.COMMAND_NAME -> ProfilerApp.MODULE_NAME))
   }
 
   override def supportedVersions: Seq[String] = Seq("1.5.0-SNAPSHOT", "1.5.0", "1.6.0-SNAPSHOT", "1.6.0")
---
+++ external/mlsql-ets/src/main/java/tech/mlsql/plugins/ets/Pivot.scala
@@ -0,0 +1,37 @@
+package tech.mlsql.plugins.ets
+
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import streaming.dsl.auth.TableAuthResult
+import streaming.dsl.mmlib.SQLAlg
+import streaming.dsl.mmlib.algs.param.WowParams
+import tech.mlsql.dsl.auth.ETAuth
+import tech.mlsql.dsl.auth.dsl.mmlib.ETMethod.ETMethod
+
+/**
+ * 8/8/2020 WilliamZhu(allwefantasy@gmail.com)
+ */
+class Pivot(override val uid: String) extends SQLAlg with ETAuth with WowParams {
+  def this() = this(Identifiable.randomUID(this.getClass.getName))
+
+  override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
+    val temp = df
+      .groupBy(params("columnLeft"))
+      .pivot(params("columnHeader"))
+
+    params("columnSum") match {
+      case "sum" => temp.sum(params("columnSum"))
+      case "count" => temp.count()
+    }
+
+  }
+
+  override def batchPredict(df: DataFrame, path: String, params: Map[String, String]): DataFrame = train(df, path, params)
+
+  override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = ???
+
+  override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = ???
+
+  override def auth(etMethod: ETMethod, path: String, params: Map[String, String]): List[TableAuthResult] = ???
+}
