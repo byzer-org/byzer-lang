27537688f5e0ad78636b4f8723076849517a6281
WilliamZhu <allwefantasy@gmail.com>
2020-08-10 11:48:37 +0800

    wow

---
+++ dev/python/convert_pom.py
@@ -0,0 +1,36 @@
+newlines = []
+
+meet = False
+meet1 = False
+
+with open("pom.xml", "r") as reader:
+    line = reader.readline()
+    while line:
+
+        if "<!-- spark 2.4 end -->" in line:
+            meet = False
+
+        if meet:
+            newlines.append("<!-- " + line.rstrip("\n") + " -->" + "\n")
+
+        if "<!-- spark 2.4 start -->" in line:
+            newlines.append(line)
+            meet = True
+
+        if "<!-- spark 3.0 end -->" in line:
+            meet1 = False
+            
+        if meet1:
+            newlines.append(line.lstrip().lstrip("<!--").rstrip().rstrip("\n").rstrip("-->") + "\n")
+
+        if "<!-- spark 3.0 start -->" in line:
+            newlines.append(line)
+            meet1 = True
+            
+        if not meet and not meet1:
+            newlines.append(line)
+
+        line = reader.readline()
+
+with open("pom.xml","w") as writer:
+    writer.writelines(newlines)
--- dev/sync.sh
+++ dev/sync.sh
@@ -1,3 +1,4 @@
 git co .
 git pull origin TRY
  ./dev/change-scala-version.sh 2.12
+python ./dev/python/convert_pom.py
