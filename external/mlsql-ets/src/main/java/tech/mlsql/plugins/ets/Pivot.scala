5960de55b779ee1196e65f5a8ac171c0e26873cc
WilliamZhu <allwefantasy@gmail.com>
2020-08-10 10:53:24 +0800

    add more features to pivot

--- external/mlsql-ets/src/main/java/tech/mlsql/plugins/ets/Pivot.scala
+++ external/mlsql-ets/src/main/java/tech/mlsql/plugins/ets/Pivot.scala
@@ -1,8 +1,9 @@
 package tech.mlsql.plugins.ets
 
+import org.apache.spark.ml.param.Param
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.expressions.UserDefinedFunction
-import org.apache.spark.sql.{DataFrame, SparkSession,functions=>F}
+import org.apache.spark.sql.{DataFrame, SparkSession, functions => F}
 import streaming.dsl.auth.TableAuthResult
 import streaming.dsl.mmlib.SQLAlg
 import streaming.dsl.mmlib.algs.param.WowParams
@@ -16,12 +17,19 @@ class Pivot(override val uid: String) extends SQLAlg with ETAuth with WowParams 
   def this() = this(Identifiable.randomUID("tech.mlsql.plugins.ets.pivot"))
 
   override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
-    val temp = df
-      .groupBy(params("columnLeft"))
-      .pivot(params("columnHeader"))
+    val groupBys = params("columnLeft").split(",")
+    var temp = df.groupBy(groupBys.head, groupBys.drop(1): _*)
 
-    val aggs = params("sunFunc").split(",").map(item=>F.expr(item).as(s"""${params("columnSum")}_${item.split("\\(").head}"""))
-    temp.agg(aggs.head,aggs.drop(1):_*)
+    if (params.get("columnHeaderFields").isDefined) {
+      val columnHeaderFields = params().split(",")
+      temp = temp.pivot(params("columnHeader"), columnHeaderFields)
+    } else {
+      temp = temp.pivot(params("columnHeader"))
+    }
+
+
+    val aggs = params("sunFunc").split(",").map(item => F.expr(item).as(s"""${params("columnSum")}_${item.split("\\(").head}"""))
+    temp.agg(aggs.head, aggs.drop(1): _*)
 
   }
 
@@ -32,4 +40,26 @@ class Pivot(override val uid: String) extends SQLAlg with ETAuth with WowParams 
   override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = ???
 
   override def auth(etMethod: ETMethod, path: String, params: Map[String, String]): List[TableAuthResult] = ???
+
+  final val columnLeft: Param[String] = new Param[String](this, "columnLeft",
+    """
+      |
+      |group by columns in pivot table.
+      |separated by comma.
+      |""".stripMargin)
+  final val columnHeaderFields: Param[String] = new Param[String](this, "columnHeaderFields",
+    """
+      |If there are too much values in  columnHeader, you can use this parameter
+      |to narrow the values.
+      |separated by comma.
+      |""".stripMargin)
+  final val columnHeader: Param[String] = new Param[String](this, "columnHeader",
+    """
+      |values in this column will be treated as new columns.
+      |""".stripMargin)
+  final val sunFunc: Param[String] = new Param[String](this, "sunFunc",
+    """
+      |agg functions, max,min,avg,sum
+      |separated by comma.
+      |""".stripMargin)
 }
