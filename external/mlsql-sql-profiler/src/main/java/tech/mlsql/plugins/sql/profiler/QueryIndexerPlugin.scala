69e1ac813b28463db4444c529c7320005befc1c5
WilliamZhu <allwefantasy@gmail.com>
2021-04-16 08:47:27 +0800

    enableQueryWithIndexer can ben set by set grammar

--- external/mlsql-sql-profiler/src/main/java/tech/mlsql/plugins/sql/profiler/QueryIndexerPlugin.scala
+++ external/mlsql-sql-profiler/src/main/java/tech/mlsql/plugins/sql/profiler/QueryIndexerPlugin.scala
@@ -10,13 +10,16 @@ import tech.mlsql.indexer.impl.{LinearTryIndexerSelector, PushdownIndexer, RestI
 class IndexerPlugin extends ResultRender {
   override def call(d: ResultResp): ResultResp = {
     val params = JSONTool.parseJson[Map[String, String]](ScriptSQLExec.context().userDefinedParam.getOrElse("__PARAMS__", "{}"))
-    if (!params.getOrElse("enableQueryWithIndexer", "false").toBoolean) {
+    val setParams = ScriptSQLExec.context().execListener.env()
+
+    if (!params.getOrElse("enableQueryWithIndexer", "false").toBoolean &&
+      setParams.getOrElse("enableQueryWithIndexer", "false").toBoolean) {
       return d
     }
     val consoleUrl = ScriptSQLExec.context().userDefinedParam.getOrElse("__default__console_url__", "")
     val auth_secret = ScriptSQLExec.context().userDefinedParam.getOrElse("__auth_secret__", "")
     val metaClient = new RestIndexerMeta(consoleUrl, auth_secret)
-    val indexer = new LinearTryIndexerSelector(Seq(new ZOrderingIndexer,new PushdownIndexer), metaClient)
+    val indexer = new LinearTryIndexerSelector(Seq(new ZOrderingIndexer, new PushdownIndexer), metaClient)
     val finalLP = indexer.rewrite(d.df.queryExecution.analyzed, Map())
     val sparkSession = ScriptSQLExec.context().execListener.sparkSession
     val ds = DataSetHelper.create(sparkSession, finalLP)
