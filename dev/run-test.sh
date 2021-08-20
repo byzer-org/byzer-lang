88f3bbf81d7e84db3bfb2b2eb3cd3894dae728ae
WilliamZhu <allwefantasy@gmail.com>
2021-08-20 17:31:43 +0800

    run-test.sh support 3.1.1
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update

--- dev/run-test.sh
+++ dev/run-test.sh
@@ -2,5 +2,18 @@
 ## example
 ## ./dev/run-test.sh
 
-mvn clean install -DskipTests
-mvn test -pl streamingpro-it
+V=${1:-2.4}
+
+if [ $V == "3.0" ];then
+   ./dev/change-scala-version.sh 2.12
+   python ./dev/python/convert_pom.py 3.0
+elif [ $V == "2.4" ]; then
+    ./dev/change-scala-version.sh 2.11
+   python ./dev/python/convert_pom.py 2.4
+else
+  echo "Only accept 2.4|3.0"
+  exit -1
+fi
+
+mvn clean install -DskipTests 
+mvn test  -pl streamingpro-it
