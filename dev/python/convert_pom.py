bc7d1163d69163c3d34b491d793efd218b438398
WilliamZhu <allwefantasy@gmail.com>
2021-08-21 22:06:41 +0800

    update

--- dev/python/convert_pom.py
+++ dev/python/convert_pom.py
@@ -1,3 +1,4 @@
+import re
 import sys
 from typing import Any, NoReturn, Callable, Dict, List
 import os
@@ -15,7 +16,114 @@ def comment(line: str):
     return "<!-- " + line.rstrip("\n") + " -->" + "\n"
 
 
-def apply(path: str, target: str) -> NoReturn:
+def cleanLine(line: str) -> str:
+    return line.rstrip("\n").strip()
+
+
+def find(lines: List[str], start: int, start_tag: str, end_tag: str, stop_tag: str) -> (int, int):
+    _start = start
+    start_tag_index = -1
+    end_tag_index = -1
+    while _start < len(lines):
+        if cleanLine(lines[_start]) == "":
+            _start += 1
+            continue
+        if cleanLine(lines[_start]) == stop_tag:
+            break
+        if cleanLine(lines[_start]) == start_tag and end_tag_index == -1:
+            start_tag_index = _start
+
+        if cleanLine(lines[_start]) == end_tag and start_tag != -1:
+            end_tag_index = _start
+        _start += 1
+    return start_tag_index, end_tag_index
+
+
+def c_activation(path: str, target: str) -> NoReturn:
+    mapping = {
+        "scala-2.11": "2.4",
+        "scala-2.12": "3.0",
+        "spark-2.4.0": "2.4",
+        "spark-3.0.0": "3.0",
+        "streamingpro-spark-2.4.0-adaptor": "2.4",
+        "streamingpro-spark-3.0.0-adaptor": "3.0"
+    }
+    with open(path, "r") as reader:
+        lines = reader.readlines()
+
+        # scala-2.11 scala-2.12
+        # spark-2.4.0 spark-3.0.0
+        # streamingpro-spark-2.4.0-adaptor streamingpro-spark-3.0.0-adaptor
+        def find_target(lines: List[str], name):
+            index = 0
+            s = -1
+            e = -1
+            while index < len(lines):
+                if index < 4:
+                    index += 1
+                    continue
+                if f"<id>{name}</id>" in lines[index]:
+                    start_tag_index, end_tag_index = find(lines, index, "<activation>", "</activation>", "</profile>")
+                    # if no <activation> found, and we should add ,then set add pos
+                    if start_tag_index == -1 and target == mapping[name]:
+                        s = index
+                        break
+                    # if <activation> is found ,and we should remove, then set remove pos
+                    if start_tag_index != -1 and end_tag_index != -1 and target != mapping[name]:
+                        s = start_tag_index
+                        e = end_tag_index
+                        break
+                index += 1
+            return s, e
+
+        def add_replace(lines: List[str], s: int, e: int):
+
+            if s == -1 and e == -1:
+                return lines
+
+            newlines = []
+            # add
+            if s != -1 and e == -1:
+                for i in range(len(lines)):
+                    newlines.append(lines[i])
+                    if i == s:
+                        newlines.append('''
+                <activation>
+                    <activeByDefault>true</activeByDefault>
+                </activation>
+''')
+
+            # replace
+            if s != -1 and e != -1:
+                for i in range(len(lines)):
+                    if i < s or i > e:
+                        newlines.append(lines[i])
+            return newlines
+
+        s, e = find_target(lines, "scala-2.11")
+        newlines = add_replace(lines, s, e)
+
+        s, e = find_target(newlines, "scala-2.12")
+        newlines = add_replace(newlines, s, e)
+
+        s, e = find_target(newlines, "spark-2.4.0")
+        newlines = add_replace(newlines, s, e)
+
+        s, e = find_target(newlines, "spark-3.0.0")
+        newlines = add_replace(newlines, s, e)
+
+
+        s, e = find_target(newlines, "streamingpro-spark-2.4.0-adaptor")
+        newlines = add_replace(newlines, s, e)
+
+        s, e = find_target(newlines, "streamingpro-spark-3.0.0-adaptor")
+        newlines = add_replace(newlines, s, e)
+
+        with open(path, "w") as writer:
+            writer.writelines(newlines)
+
+
+def c_properties(path: str, target: str) -> NoReturn:
     newlines = []
 
     meet2_4 = False
@@ -67,4 +175,5 @@ for root, dirs, files in os.walk("."):
         if file.endswith("pom.xml"):
             file_path = os.path.join(root, file)
             print(f"Apply {target} in  {file_path}")
-            apply(file_path, target)
+            c_properties(file_path, target)
+            c_activation(file_path, target)
