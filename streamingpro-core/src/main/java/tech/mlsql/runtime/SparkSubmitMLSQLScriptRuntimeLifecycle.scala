eb9d637843be45d03e68c5db8de4fa2cd0fb0fd6
jiachuan.zhu <jiachuan.zhu@kyligence.io>
2021-09-07 15:13:29 +0800

    fix: SparkSubmitMLSQLScriptRuntimeLifecycle should extend MLSQLPlatformLifecycle

--- streamingpro-core/src/main/java/tech/mlsql/runtime/SparkSubmitMLSQLScriptRuntimeLifecycle.scala
+++ streamingpro-core/src/main/java/tech/mlsql/runtime/SparkSubmitMLSQLScriptRuntimeLifecycle.scala
@@ -1,8 +1,24 @@
 package tech.mlsql.runtime
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import org.apache.commons.lang3.StringUtils
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import streaming.core.strategy.platform.{SparkRuntime, StreamingRuntime}
 import tech.mlsql.common.utils.log.Logging
 import tech.mlsql.job.{JobManager, RunScriptExecutor}
 
@@ -17,16 +33,22 @@ import scala.io.Source
  * streaming.mlsql.script.owner=XXXXX
  * streaming.mlsql.script.jobName=SparkSubmitTest
  */
-class SparkSubmitMLSQLScriptRuntimeLifecycle extends MLSQLRuntimeLifecycle with Logging {
+class SparkSubmitMLSQLScriptRuntimeLifecycle extends MLSQLPlatformLifecycle with Logging {
 
-  override def beforeRuntimeStarted(params: Map[String, String], conf: SparkConf): Unit = {}
+  override def beforeRuntime(params: Map[String, String]): Unit = {}
 
-  override def afterRuntimeStarted(params: Map[String, String], conf: SparkConf, rootSparkSession: SparkSession): Unit = {
+  override def afterRuntime(runtime: StreamingRuntime, params: Map[String, String]): Unit = {  }
+
+
+  override def beforeDispatcher(runtime: StreamingRuntime, params: Map[String, String]): Unit = {}
+
+  override def afterDispatcher(runtime: StreamingRuntime, params: Map[String, String]): Unit = {
     val mlsql_path = params.getOrElse("streaming.mlsql.script.path", "")
     if (StringUtils.isEmpty(mlsql_path)) {
       logWarning(s"The value of parameter 'streaming.mlsql.script.path' is empty.")
       return
     }
+    val rootSparkSession = runtime.asInstanceOf[SparkRuntime].sparkSession
     JobManager.init(rootSparkSession)
     try {
       val sql = new StringBuffer()
