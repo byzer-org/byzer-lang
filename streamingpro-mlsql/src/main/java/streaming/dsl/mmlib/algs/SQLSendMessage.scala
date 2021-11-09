0eea96395c8b17dbc4e08fad26db78a874aafd01
hellozepp <jingtianyy@qq.com>
2021-11-09 18:57:39 +0800

    support html mail format support mail attachments

--- streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLSendMessage.scala
+++ streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLSendMessage.scala
@@ -18,19 +18,29 @@
 
 package streaming.dsl.mmlib.algs
 
+import org.apache.commons.lang.StringUtils
 import java.util.{Date, Properties}
-import javax.mail.internet.{InternetAddress, MimeMessage}
-import javax.mail.{Address, Authenticator, Message, PasswordAuthentication, Session, Transport}
+import javax.mail.internet.{ContentType, InternetAddress, MimeBodyPart, MimeMessage, MimeMultipart, MimeUtility}
+import javax.mail.{Address, Authenticator, BodyPart, Message, Multipart, PasswordAuthentication, Session, Transport}
 import org.apache.spark.ml.param.Param
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import streaming.dsl.ScriptSQLExec
 import streaming.dsl.mmlib.{Code, Doc, HtmlDoc, SQLAlg, SQLCode}
 import streaming.dsl.mmlib.algs.SQLSendMessage.MailTypeEnum.MailTypeEnum
-import streaming.dsl.mmlib.algs.SQLSendMessage._
+import streaming.dsl.mmlib.algs.SQLSendMessage.{MailContentTypeEnum, _}
 import streaming.dsl.mmlib.algs.param.BaseParams
 import streaming.log.WowLog
 import tech.mlsql.common.form.{Dynamic, Extra, FormParams, KV, Select, Text}
 import tech.mlsql.common.utils.log.Logging
+import tech.mlsql.dsl.adaptor.DslTool
+import tech.mlsql.tool.HDFSOperatorV2
+
+import java.io.File
+import java.nio.file.Paths
+import javax.activation.DataHandler
+import javax.mail.util.ByteArrayDataSource
+import scala.collection.mutable
 
 /**
  * Created by fchen on 2018/8/22.
@@ -50,7 +60,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
         label = "the sender",
         options = Map(
           "valueType" -> "string",
-          "defaultValue" -> "",
           "required" -> "true",
           "derivedType" -> "NONE"
         )), valueProvider = Option(() => {
@@ -59,7 +68,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-
   val to: Param[String] = new Param[String](this, "to",
     FormParams.toJson(Text(
       name = "to",
@@ -73,7 +81,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
         label = "The target email addresses",
         options = Map(
           "valueType" -> "string",
-          "defaultValue" -> "",
           "required" -> "true",
           "derivedType" -> "NONE"
         )), valueProvider = Option(() => {
@@ -82,7 +89,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-
   val cc: Param[String] = new Param[String](this, "cc",
     FormParams.toJson(Text(
       name = "cc",
@@ -96,7 +102,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
         label = "The email address of the CC people",
         options = Map(
           "valueType" -> "string",
-          "defaultValue" -> "",
           "required" -> "false",
           "derivedType" -> "NONE"
         )), valueProvider = Option(() => {
@@ -105,7 +110,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-
   val subject: Param[String] = new Param[String](this, "subject",
     FormParams.toJson(Text(
       name = "subject",
@@ -118,7 +122,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
         label = "The title of email",
         options = Map(
           "valueType" -> "string",
-          "defaultValue" -> "",
           "required" -> "false",
           "derivedType" -> "NONE"
         )), valueProvider = Option(() => {
@@ -127,7 +130,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-
   val smtpHost: Param[String] = new Param[String](this, "smtpHost",
     FormParams.toJson(Dynamic(
       name = "smtpHost",
@@ -147,7 +149,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
       valueProviderName = ""
     )
     ))
-
   val smtpPort: Param[String] = new Param[String](this, "smtpPort",
     FormParams.toJson(Dynamic(
       name = "smtpPort",
@@ -167,7 +168,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
       valueProviderName = ""
     )
     ))
-
   val method: Param[String] = new Param[String](this, "method",
     FormParams.toJson(Select(
       name = "method",
@@ -180,7 +180,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
         label = "Way of sending",
         options = Map(
           "valueType" -> "string",
-          "defaultValue" -> "",
           "required" -> "true",
           "derivedType" -> "NONE"
         )), valueProvider = Option(() => {
@@ -189,7 +188,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-
   val mailType: Param[String] = new Param[String](this, "mailType",
     FormParams.toJson(Select(
       name = "mailType",
@@ -215,8 +213,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-  setDefault(mailType, "config")
-
   val userName: Param[String] = new Param[String](this, "userName",
     FormParams.toJson(Dynamic(
       name = "userName",
@@ -234,7 +230,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
       valueProviderName = ""
     )
     ))
-
   val password: Param[String] = new Param[String](this, "password",
     FormParams.toJson(Dynamic(
       name = "password",
@@ -252,7 +247,7 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
       valueProviderName = ""
     )
     ))
-
+  setDefault(mailType, "config")
   val content: Param[String] = new Param[String](this, "content",
     FormParams.toJson(Text(
       name = "content",
@@ -260,13 +255,12 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
       extra = Extra(
         doc =
           """
-            | Required. The server of email
+            | Required. The content of email
           """,
-        label = "The email server",
+        label = "The email content",
         options = Map(
           "valueType" -> "string",
-          "defaultValue" -> "",
-          "required" -> "true",
+          "required" -> "false",
           "derivedType" -> "NONE"
         )), valueProvider = Option(() => {
         ""
@@ -274,7 +268,99 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
     )
     )
   )
-
+  val contentType: Param[String] = new Param[String](this, "contentType",
+    FormParams.toJson(Select(
+      name = "contentType",
+      values = List(),
+      extra = Extra(
+        doc =
+          """
+            | Format used to send email Content-Type
+          """,
+        label = "Format used to send email Content-Type",
+        options = Map(
+          "valueType" -> "string",
+          "required" -> "false",
+          "derivedType" -> "NONE"
+        )), valueProvider = Option(() => {
+        List(KV(Option("contentType"), Option(MailContentTypeEnum.MIXED.toString)))
+        List(KV(Option("contentType"), Option(MailContentTypeEnum.TEXT.toString)))
+        List(KV(Option("contentType"), Option(MailContentTypeEnum.HTML.toString)))
+        List(KV(Option("contentType"), Option(MailContentTypeEnum.CSV.toString)))
+        List(KV(Option("contentType"), Option(MailContentTypeEnum.JPEG.toString)))
+        List(KV(Option("contentType"), Option(MailContentTypeEnum.DEFAULT_ATTACHMENT.toString)))
+      })
+    )
+    )
+  )
+  setDefault(contentType, MailContentTypeEnum.TEXT.toString)
+  val attachmentContentType: Param[String] = new Param[String](this, "attachmentContentType",
+    FormParams.toJson(Select(
+      name = "attachmentContentType",
+      values = List(),
+      extra = Extra(
+        doc =
+          """
+            | Format used to send email attachment Content-Type
+          """,
+        label = "Format used to send email content",
+        options = Map(
+          "valueType" -> "string",
+          "required" -> "false",
+          "derivedType" -> "NONE"
+        )), valueProvider = Option(() => {
+        List(KV(Option("attachmentContentType"), Option(MailContentTypeEnum.MIXED.toString)))
+        List(KV(Option("attachmentContentType"), Option(MailContentTypeEnum.TEXT.toString)))
+        List(KV(Option("attachmentContentType"), Option(MailContentTypeEnum.HTML.toString)))
+        List(KV(Option("attachmentContentType"), Option(MailContentTypeEnum.CSV.toString)))
+        List(KV(Option("attachmentContentType"), Option(MailContentTypeEnum.JPEG.toString)))
+        List(KV(Option("attachmentContentType"), Option(MailContentTypeEnum.DEFAULT_ATTACHMENT.toString)))
+      })
+    )
+    )
+  )
+  setDefault(attachmentContentType, MailContentTypeEnum.DEFAULT_ATTACHMENT.toString)
+  val attachmentPaths: Param[String] = new Param[String](this, "attachmentPaths",
+    FormParams.toJson(Text(
+      name = "attachmentPaths",
+      value = "",
+      extra = Extra(
+        doc =
+          """
+            | The file address of the attachment, multiple addresses are separated by','.
+          """,
+        label = "The email content",
+        options = Map(
+          "valueType" -> "string",
+          "required" -> "false",
+          "derivedType" -> "NONE"
+        )), valueProvider = Option(() => {
+        ""
+      })
+    )
+    )
+  )
+  val charset: Param[String] = new Param[String](this, "charset",
+    FormParams.toJson(Text(
+      name = "charset",
+      value = "",
+      extra = Extra(
+        doc =
+          """
+            | The charset of email content
+          """,
+        label = "The charset of email content",
+        options = Map(
+          "valueType" -> "string",
+          "required" -> "false",
+          "derivedType" -> "NONE"
+        )), valueProvider = Option(() => {
+        ""
+      })
+    )
+    )
+  )
+  setDefault(charset, "utf-8")
   def this() = this(BaseParams.randomUID())
 
   override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = {
@@ -290,34 +376,39 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
   }
 
   override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
-    val messageCount = df.count()
-    require(messageCount <= MAX_MESSAGE_THRESHOLD, s"message count should <= $MAX_MESSAGE_THRESHOLD!")
-
+    val content = params.getOrElse("content", "")
     val to = params("to")
     val from = params.getOrElse("from", null)
     val cc = params.getOrElse("cc", null)
     val subject = params.getOrElse("subject", "no subject!")
     val method = params("method")
-    require(to != null, "the parameter [to] cannot be empty!")
-    val agent = new MailAgent()
-    if (df.count() == 0) {
-      method.toUpperCase() match {
-        case "MAIL" =>
-          logInfo(format(s"send content: ${$(content)} to $to"))
-          agent.sendMessage(to, cc, null, from, subject, $(content), params)
-        case _ =>
-          throw new RuntimeException("unsupported method!")
-      }
-    } else {
-      df.toJSON.collect().foreach(contentJson => {
-        method.toUpperCase() match {
-          case "MAIL" =>
-            logInfo(format(s"send content: $contentJson to $to"))
-            agent.sendMessage(to, cc, null, from, subject, contentJson, params)
-          case _ =>
-            throw new RuntimeException("unsupported method!")
+    val charset = params.getOrElse("charset", "utf-8")
+    val contentType = params.getOrElse("contentType", MailContentTypeEnum.TEXT.toString)
+    val attachmentContentType = params.getOrElse("attachmentContentType", MailContentTypeEnum.DEFAULT_ATTACHMENT.toString)
+    val attachmentPaths = params.getOrElse("attachmentPaths", null)
+    val attachmentMap = mutable.Map.empty[String, (String, String)]
+    if (attachmentPaths != null) {
+      attachmentPaths.split(",").foreach(path => {
+        if (StringUtils.isNotBlank(path) && path.lastIndexOf(File.separatorChar) != -1) {
+          val separatorIndex = path.lastIndexOf(File.separatorChar)
+          val filePath = path.substring(0, separatorIndex + 1)
+          val fileName = path.substring(separatorIndex + 1)
+          attachmentMap.put(path, filePath -> fileName)
+        } else if (StringUtils.isNotBlank(path)) {
+          attachmentMap.put(path, path -> path)
         }
       })
+    }
+
+    require(to != null, "the parameter [to] cannot be empty!")
+    val agent = new MailAgent()
+    method.toUpperCase() match {
+      case "MAIL" =>
+        logInfo(format(s"send content: $content to $to"))
+        agent.sendMessage(to, cc, null, from, subject, content, params,
+          contentType, attachmentContentType, attachmentMap.toMap, charset)
+      case _ =>
+        throw new RuntimeException("unsupported method!")
     }
     logInfo(format("Email sent successfully!"))
     emptyDataFrame()(df)
@@ -340,7 +431,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
       | Use "load modelExample.`SendMessage` as output;"
       | get example.
     """.stripMargin)
-
 
   override def codeExample: Code = Code(SQLCode,
     """
@@ -387,8 +477,6 @@ class SQLSendMessage(override val uid: String) extends SQLAlg with Functions wit
 }
 
 object SQLSendMessage {
-  val MAX_MESSAGE_THRESHOLD = 10
-
   private[algs] object MailTypeEnum extends Enumeration {
     type MailTypeEnum = Value
     val CONFIG: Value = Value("config")
@@ -396,37 +484,110 @@ object SQLSendMessage {
 
     def checkExists(optionValues: String): Boolean = this.values.exists(_.toString == optionValues)
   }
+
+  private[algs] object MailContentTypeEnum extends Enumeration {
+    type MailContentTypeEnum = Value
+    val MIXED: Value = Value("multipart/mixed")
+    val TEXT: Value = Value("text/plain")
+    val HTML: Value = Value("text/html")
+    val CSV: Value = Value("text/csv")
+    val JPEG: Value = Value("image/jpeg")
+    val DEFAULT_ATTACHMENT: Value = Value("application/octet-stream")
+    // Does not support the use of cascading relationships in the email content, including types: RELATED, ALTERNATIVE
+    val RELATED: Value = Value("multipart/related")
+    val ALTERNATIVE: Value = Value("multipart/alternative")
+
+    def checkExists(optionValues: String): Boolean = this.values.exists(_.toString == optionValues)
+  }
 }
 
-class MailAgent() extends Logging with WowLog {
+class MailAgent() extends Logging with WowLog with DslTool {
+
   // If the value is set, the parameter description can be changed to: required" -> "false"
   final val GLOBAL_USER_NAME: String = null
   final val GLOBAL_IDENTIFY_CODE: String = null
 
-  // throws MessagingException
   def sendMessage(to: String,
                   cc: String,
                   bcc: String,
                   from: String,
                   subject: String,
                   content: String,
-                  params: Map[String, String]): Unit = {
-    var message: Message = null
-    val mailType = params.getOrElse("mailType", "config")
+                  params: Map[String, String],
+                  mailContentType: String,
+                  attachmentContentType: String,
+                  attachmentMap: Map[String, (String, String)],
+                  charset: String): Unit = {
+    //Build the header
+    val message: Message = buildMessageHead(params, from, to, cc, bcc)
+
+    //Build the body
+    val body: Multipart = new MimeMultipart
+
+    // Set Content-Type
+    val bodyCt = buildContentType(mailContentType, charset)
+
+    //Build these contents
+    val contentPart = new MimeBodyPart
+    contentPart.setContent(content, bodyCt.toString)
+    body.addBodyPart(contentPart)
+
+    //Set some attachment files to Message including the Content-Type
+    if (attachmentMap != null && attachmentMap.nonEmpty) {
+      import scala.collection.JavaConversions._
+      for (entry <- attachmentMap.entrySet) {
+        val (filePath, fileName) = entry.getValue
+        val context = ScriptSQLExec.contextGetOrForTest()
+        val baseDir = resourceRealPath(context.execListener, Option(context.owner), filePath)
+        if (HDFSOperatorV2.isDir(baseDir) && HDFSOperatorV2.fileExists(Paths.get(baseDir, fileName).toString)) {
+          body.addBodyPart(buildAttachmentBody(baseDir, fileName, attachmentContentType))
+        }
+      }
+    }
+
+    // Set Multipart-Content to Message including the Content-Type
+    message.setContent(body, buildContentType(MailContentTypeEnum.MIXED.toString, charset).toString)
+    message.setSubject(subject)
+    Transport.send(message)
+  }
+
+  def buildContentType(mailContentType: String, charset: String): ContentType = {
+    val ct = new ContentType(mailContentType)
+    ct.setParameter("charset", charset)
+    ct
+  }
+
+  /**
+   * Build the attachment body of the email
+   *
+   * @param filePath              the path of attachment file
+   * @param fileName              the name of attachment file
+   * @param attachmentContentType the Content-Type of the attachment file
+   * @return
+   * @throws Exception
+   */
+  @throws[Exception]
+  private def buildAttachmentBody(filePath: String, fileName: String, attachmentContentType: String): BodyPart = {
+    val body: BodyPart = new MimeBodyPart
+    body.setDataHandler(new DataHandler(new ByteArrayDataSource(
+      HDFSOperatorV2.readAsInputStream(Paths.get(filePath, fileName).toString), attachmentContentType)))
+    body.setFileName(MimeUtility.encodeWord(fileName))
+    body
+  }
+
+  @throws[Exception]
+  def buildMessageHead(params: Map[String, String], from: String, to: String, cc: String, bcc: String): MimeMessage = {
+    val mailType = params.getOrElse("mailType", MailTypeEnum.CONFIG.toString)
     assert(SQLSendMessage.MailTypeEnum.checkExists(mailType), s"Unsupported mailType: $mailType!")
     val session = createSession(SQLSendMessage.MailTypeEnum.withName(mailType), params)
-    message = new MimeMessage(session)
+    val message = new MimeMessage(session)
     if (from != null) {
       message.setFrom(new InternetAddress(from))
     }
     setToCcBccRecipients(message, to, cc, bcc)
-
     message.setSentDate(new Date())
-    message.setSubject(subject)
-    message.setText(content)
-    Transport.send(message)
+    message
   }
-
 
   def createSession(mailType: MailTypeEnum, params: Map[String, String]): Session = {
     val properties = new Properties
@@ -454,7 +615,6 @@ class MailAgent() extends Logging with WowLog {
     Session.getInstance(properties, null)
   }
 
-  // throws AddressException, MessagingException
   def setToCcBccRecipients(message: Message, to: String, cc: String, bcc: String): Unit = {
     setMessageRecipients(message, to, Message.RecipientType.TO)
     if (cc != null) {
