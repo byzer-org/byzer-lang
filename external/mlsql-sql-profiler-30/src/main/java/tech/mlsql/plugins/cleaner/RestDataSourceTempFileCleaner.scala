ca4f4c862b0bd4d9a783aa921a83bbfea41a4dcd
WilliamZhu <allwefantasy@gmail.com>
2021-12-02 22:26:16 +0800

    add clean job for Rest datasource (#32)

---
+++ external/mlsql-sql-profiler-30/src/main/java/tech/mlsql/plugins/cleaner/RestDataSourceTempFileCleaner.scala
@@ -0,0 +1,39 @@
+package tech.mlsql.plugins.cleaner
+
+import streaming.dsl.ScriptSQLExec
+import streaming.log.WowLog
+import tech.mlsql.app.RequestCleaner
+import tech.mlsql.common.utils.log.Logging
+import tech.mlsql.common.utils.serder.json.JSONTool
+import tech.mlsql.tool.HDFSOperatorV2
+
+/**
+ * 2/12/2021 WilliamZhu(allwefantasy@gmail.com)
+ */
+class RestDataSourceTempFileCleaner extends RequestCleaner with Logging with WowLog {
+
+  val NAME = "tech.mlsql.datasource.impl.MLSQLRest"
+
+  override def run(): Unit = {
+    val context = ScriptSQLExec.context()
+    val ifAsync = JSONTool.parseJson[Map[String, String]](context.userDefinedParam("__PARAMS__")).getOrElse("async", "false").toBoolean
+    if (ifAsync && !context.execListener.env().getOrElse("__MarkAsyncRunFinish__", "false").toBoolean) {
+      return
+    }
+
+    context.execListener.env().get(NAME) match {
+      case Some(dirs) =>
+        dirs.split(",").foreach { dir =>
+          logInfo(s"${NAME}: clean ${dir}")
+          try {
+            HDFSOperatorV2.deleteDir(dir)
+          } catch {
+            case e: Exception =>
+              logInfo(s"Fail to clean ${dir}", e)
+          }
+
+        }
+      case None =>
+    }
+  }
+}
