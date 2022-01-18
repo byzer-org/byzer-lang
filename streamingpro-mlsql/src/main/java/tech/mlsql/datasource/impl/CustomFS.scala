0638c3e9e1c6931320ccee4e9e2923cef290b200
jiachuan.zhu <jiachuan.zhu@kyligence.io>
2022-01-18 17:00:54 +0800

    Azure blob config does not require spark.hadoop prefix in CustomFS

--- streamingpro-mlsql/src/main/java/tech/mlsql/datasource/impl/CustomFS.scala
+++ streamingpro-mlsql/src/main/java/tech/mlsql/datasource/impl/CustomFS.scala
@@ -24,6 +24,7 @@ class CustomFS(override val uid: String) extends MLSQLSource
     require(URI.create(config.path).getScheme!=null,"path should be with schema specified")
 
     val session = config.df.get.sparkSession
+    // Object store config starts with spark.hadoop or fs
     val (objectStoreConf,loadFileConf) = config.config.partition(item => item._1.startsWith("spark.hadoop") || item._1.startsWith("fs."))
 
     objectStoreConf.map { item =>
@@ -31,10 +32,14 @@ class CustomFS(override val uid: String) extends MLSQLSource
         ("spark.hadoop." + item._1, item._2)
       } else item
     }.foreach(item => session.conf.set(item._1, item._2))
+    // To load azure blob data, spark.hadoop prefix should be removed. Because NativeAzureFileSystem - azure blob's HDFS
+    // compatible API, looks up Azure blob credentials by fs.azure.account.key.<account>.blob.core.chinacloudapi.cn.
+    objectStoreConf.filter(_._1.startsWith("spark.hadoop.fs.azure")).foreach{ conf =>
+      session.conf.set( conf._1.replace("spark.hadoop.fs.azure", "fs.azure"), conf._2)
+    }
 
     val format = config.config.getOrElse("implClass", fullFormat)
     reader.options(loadFileConf).format(format).load(config.path)
-    
   }
 
   override def save(writer: DataFrameWriter[Row], config: DataSinkConfig): Any = {
@@ -49,6 +54,10 @@ class CustomFS(override val uid: String) extends MLSQLSource
         ("spark.hadoop." + item._1, item._2)
       } else item
     }.foreach(item => session.conf.set(item._1, item._2))
+    // See comments in load method
+    objectStoreConf.filter(_._1.startsWith("spark.hadoop.fs.azure")).foreach{ conf =>
+      session.conf.set( conf._1.replace("spark.hadoop.fs.azure", "fs.azure"), conf._2)
+    }
 
     val format = config.config.getOrElse("implClass", fullFormat)
     writer.options(loadFileConf).mode(config.mode).format(format).save(config.path)
