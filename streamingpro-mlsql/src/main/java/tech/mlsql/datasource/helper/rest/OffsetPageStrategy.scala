1857d6649df6ae532d93847abb9c216767971a4f
admondguo <admondguo@163.com>
2022-02-14 10:12:05 +0800

    Add rest PageStrategy OffsetPageStrategy.

---
+++ streamingpro-mlsql/src/main/java/tech/mlsql/datasource/helper/rest/OffsetPageStrategy.scala
@@ -0,0 +1,32 @@
+package tech.mlsql.datasource.helper.rest
+
+import org.apache.spark.sql.mlsql.session.MLSQLException
+import tech.mlsql.tool.Templates2
+
+class OffsetPageStrategy(params: Map[String, String]) extends PageStrategy {
+  val Array(_, pageParam) = params("config.page.values") match {
+    case s if s.contains(":") => s.split(":")
+    case _ => throw new MLSQLException("`config.page.values` should contains ':'")
+  }
+
+  val Array(initialPageNum, pageIncrement) = pageParam match {
+    case s if s.contains(",") => pageParam.split(",")
+    case _ => Array(pageParam, "1")
+  }
+
+  var pageNum: Int = initialPageNum.trim.toInt
+  val pageAddend: Int = pageIncrement.trim.toInt
+
+  override def pageValues(_content: Option[Any]) = Array[String](pageNum.toString)
+
+
+  override def nexPage: OffsetPageStrategy = {
+    pageNum += pageAddend
+    this
+  }
+
+  override def pageUrl(_content: Option[Any]): String = {
+    val urlTemplate = params("config.page.next")
+    Templates2.evaluate(urlTemplate, pageValues(None))
+  }
+}
