afaef63f2b63b13076e83ae92d042c551de2b0f3
AndieHuang <ckeys1993@gmail.com>
2022-04-18 20:57:31 +0800

    add unit test for master slave in spark

--- streamingpro-mlsql/src/test/scala/tech/mlsql/test/MasterSlaveInSparkTest.scala
+++ streamingpro-mlsql/src/test/scala/tech/mlsql/test/MasterSlaveInSparkTest.scala
@@ -4,7 +4,7 @@ import org.apache.arrow.vector.VectorSchemaRoot
 import org.apache.arrow.vector.ipc.ArrowStreamReader
 import org.apache.spark.{TaskContext, WowRowEncoder}
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
 import org.apache.spark.sql.vectorized.{ArrowColumnVector, ColumnVector, ColumnarBatch}
 import org.apache.spark.streaming.{BasicSparkOperation, SparkOperationUtil}
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
@@ -17,6 +17,7 @@ import tech.mlsql.arrow.python.iapp.{AppContextImpl, JavaContext}
 import tech.mlsql.arrow.python.ispark.SparkContextImp
 import tech.mlsql.arrow.python.runner.{ArrowPythonRunner, ChainedPythonFunctions, PythonConf, PythonFunction, ReaderIterator, SparkSocketRunner, SpecialLengths}
 import tech.mlsql.common.utils.lang.sc.ScalaMethodMacros
+import tech.mlsql.common.utils.log.Logging
 import tech.mlsql.common.utils.network.NetUtils
 import tech.mlsql.ets.ray.DataServer
 import tech.mlsql.nativelib.runtime.NativeFuncRule
@@ -69,17 +70,17 @@ class MasterSlaveInSparkTest extends FunSuite with SparkOperationUtil with SpecF
       import session.implicits._
       val timezoneID = session.sessionState.conf.sessionLocalTimeZone
       val newdf = session.createDataset[DataServer](modelServer.dataServers.get().map(f => DataServer(f.host, f.port, timezoneID))).repartition(1)
+      val schema = StructType(Seq(StructField("name", StringType), StructField("age", IntegerType), StructField("phone", LongType)))
+      val convert = WowRowEncoder.toRow(schema)
       val res = newdf.rdd.flatMap { row =>
-        val socketRunner = new SparkSocketRunner("readFromStreamWithArrow", NetUtils.getHost, timezoneID)
         val host = row.host
         val port = row.port
-        //        val worker = new Socket(host, port)
-        //        val stream = new DataInputStream(worker.getInputStream)
-        val commonTaskContext = new SparkContextImp(TaskContext.get(), null)
-        val iter = socketRunner.readFromStreamWithArrow(host, port, commonTaskContext)
+        val iter = ArrowReaderUtils.readFromStreamWithArrow(host, port)
         iter.map(f => f.copy())
       }.collect()
-      print(res.length)
+      res.foreach { item =>
+        println(convert(item))
+      }
     }
   }
 
@@ -133,8 +134,97 @@ class MasterSlaveInSparkTest extends FunSuite with SparkOperationUtil with SpecF
 
 }
 
-class Reader() {
-  def read(): Unit = {
+abstract class ReaderIteratorForTest[OUT](
+                                           stream: DataInputStream,
+                                           startTime: Long)
+  extends Iterator[OUT] with Logging {
 
+  private var nextObj: OUT = _
+  private var eos = false
+
+  override def hasNext: Boolean = nextObj != null || {
+    if (!eos) {
+      nextObj = read()
+      if (nextObj == null) return false
+      hasNext
+    } else {
+      false
+    }
+  }
+
+  override def next(): OUT = {
+    if (hasNext) {
+      val obj = nextObj
+      nextObj = null.asInstanceOf[OUT]
+      obj
+    } else {
+      Iterator.empty.next()
+    }
+  }
+
+  /**
+   * Reads next object from the stream.
+   * When the stream reaches end of data, needs to process the following sections,
+   * and then returns null.
+   */
+  protected def read(): OUT
+}
+
+object ArrowReaderUtils {
+  def readFromStreamWithArrow(host: String, port: Int) = {
+    val socket = new Socket(host, port)
+    val stream = new DataInputStream(socket.getInputStream)
+    new ReaderIteratorForTest[ColumnarBatch](stream, System.currentTimeMillis()) {
+      private val allocator = ArrowUtils.rootAllocator.newChildAllocator(
+        s"stdin reader ", 0, Long.MaxValue)
+
+      private var reader: ArrowStreamReader = _
+      private var root: VectorSchemaRoot = _
+      private var schema: StructType = _
+      private var vectors: Array[ColumnVector] = _
+
+      private var batchLoaded = true
+
+      protected override def read(): ColumnarBatch = {
+
+        if (reader != null && batchLoaded) {
+          batchLoaded = reader.loadNextBatch()
+          if (batchLoaded) {
+            val batch = new ColumnarBatch(vectors)
+            batch.setNumRows(root.getRowCount)
+            batch
+          } else {
+            reader.close(false)
+            allocator.close()
+            // Reach end of stream. Call `read()` again to read control data.
+            null
+          }
+        } else {
+          reader = new ArrowStreamReader(stream, allocator)
+          root = try {
+            // in case we have a empty stream
+            reader.getVectorSchemaRoot()
+          } catch {
+            case e: Exception =>
+              batchLoaded = false
+              reader.close(false)
+              allocator.close()
+              // Reach end of stream. Call `read()` again to read control data.
+              null
+          }
+          if (root == null) {
+            return null
+          }
+          schema = ArrowUtils.fromArrowSchema(root.getSchema())
+          vectors = root.getFieldVectors().asScala.map { vector =>
+            new ArrowColumnVector(vector)
+          }.toArray[ColumnVector]
+          read()
+        }
+
+      }
+    }.flatMap { batch =>
+      batch.rowIterator.asScala
+    }
   }
 }
