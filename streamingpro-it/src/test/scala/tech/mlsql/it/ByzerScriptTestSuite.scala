783d1f87832da2e70edcc69fbc0523a12d9c6ad6
hellozepp <jingtianyy@qq.com>
2022-08-10 09:43:47 +0800

    Support callback header (#1808)
    * Support runscript callbackHeader

--- streamingpro-it/src/test/scala/tech/mlsql/it/ByzerScriptTestSuite.scala
+++ streamingpro-it/src/test/scala/tech/mlsql/it/ByzerScriptTestSuite.scala
@@ -1,5 +1,8 @@
 package tech.mlsql.it
 
+import net.csdn.modules.transport.DefaultHttpTransportService
+import org.apache.http.HttpEntity
+import org.apache.http.util.EntityUtils
 import tech.mlsql.common.utils.log.Logging
 import tech.mlsql.crawler.RestUtils
 import tech.mlsql.it.contiainer.ByzerCluster
@@ -8,6 +11,7 @@ import tech.mlsql.it.utils.DockerUtils.getCurProjectRootPath
 
 import java.io.File
 import java.util.UUID
+import scala.collection.mutable
 
 /**
  * 23/02/2022 hellozepp(lisheng.zhanglin@163.com)
@@ -54,11 +58,27 @@ class ByzerScriptTestSuite extends LocalBaseTestSuite with Logging {
     })
   }
 
-  def runScript(url: String, user: String, code: String): (Int, String) = {
+  def runScript(url: String, user: String, code: String, callbackHeader: String = ""): (Int, String) = {
     val jobName = UUID.randomUUID().toString
+    val params = mutable.Map("sql" -> code, "owner" -> user,
+      "jobName" -> jobName, "sessionPerUser" -> "true", "sessionPerRequest" -> "true")
+    if (callbackHeader != "") params.put("callbackHeader", callbackHeader)
     logInfo(s"The test submits a script to the container through Rest, url:$url, sql:$code")
-    val (status, result) = RestUtils.rest_request_string(url, "post", Map("sql" -> code, "owner" -> user,
-      "jobName" -> jobName, "sessionPerUser" -> "true", "sessionPerRequest" -> "true"),
+    val (status, result) = RestUtils.rest_request_string(url, "post", params.toMap,
+      Map("Content-Type" -> "application/x-www-form-urlencoded"), Map("socket-timeout" -> "1800s",
+        "connect-timeout" -> "1800s", "retry" -> "1")
+    )
+    logInfo(s"status:$status,result:$result")
+    (status, result)
+  }
+
+  def runScriptWithHeader(url: String, user: String, code: String, callbackHeader: String = ""): (Int, HttpEntity) = {
+    val jobName = UUID.randomUUID().toString
+    val params = mutable.Map("sql" -> code, "owner" -> user,
+      "jobName" -> jobName, "sessionPerUser" -> "true", "sessionPerRequest" -> "true")
+    if (callbackHeader != "") params.put("callbackHeader", callbackHeader)
+    logInfo(s"The test submits a script to the container through Rest, url:$url, sql:$code")
+    val (status, result) = RestUtils.rest_request(url, "post", params.toMap,
       Map("Content-Type" -> "application/x-www-form-urlencoded"), Map("socket-timeout" -> "1800s",
         "connect-timeout" -> "1800s", "retry" -> "1")
     )
@@ -82,7 +102,7 @@ class ByzerScriptTestSuite extends LocalBaseTestSuite with Logging {
     val cluster: ByzerCluster = setupCluster()
     val hadoopContainer = cluster.hadoopContainer
     val byzerLangContainer = cluster.byzerLangContainer
-    val javaContainer = cluster.byzerLangContainer.container
+    val javaContainer = byzerLangContainer.container
     url = s"http://${javaContainer.getHost}:${javaContainer.getMappedPort(9003)}/run/script"
 
     test("javaContainer") {
@@ -101,6 +121,19 @@ class ByzerScriptTestSuite extends LocalBaseTestSuite with Logging {
     }
 
     test("Execute yarn sql file") {
+      try {
+        val (_, result) = runScriptWithHeader(url, user, "select 1 as a,'jack' as b as bbc;",
+          """{"Authorization":"Bearer acc"}""")
+        val _result = EntityUtils.toString(result, DefaultHttpTransportService.charset)
+        println("With callbackHeader result:" + _result)
+        assert(_result === "[{\"a\":1,\"b\":\"jack\"}]")
+      } catch {
+        case _: Exception =>
+          val res = "callbackHeader should be returned normally in the byzer callback!"
+          logError(res)
+          throw new RuntimeException(res)
+      }
+
       TestManager.testCases.foreach(testCase => {
         try {
           val (status, result) = runScript(url, user, testCase.sql)
@@ -110,7 +143,6 @@ class ByzerScriptTestSuite extends LocalBaseTestSuite with Logging {
             TestManager.acceptRest(testCase, 500, null, e)
         }
       })
-
       TestManager.report()
     }
 
