d08b7046a071f3c3b5a33886c4f98ce854d09149
WilliamZhu <allwefantasy@gmail.com>
2022-08-19 12:16:01 +0800

    enhance ET jdbc (#1817)
    optimize import
    
    fix bug

--- streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLJDBC.scala
+++ streamingpro-mlsql/src/main/java/streaming/dsl/mmlib/algs/SQLJDBC.scala
@@ -16,44 +16,50 @@
  * limitations under the License.
  */
 
-
 package streaming.dsl.mmlib.algs
 
-
-import com.alibaba.druid.util.JdbcConstants
 import net.sf.json.JSONObject
 import org.apache.spark.ml.param.Param
-import org.apache.spark.sql.execution.MLSQLAuthParser
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.mlsql.session.MLSQLException
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import streaming.core.datasource.{DataAuthConfig, JDBCUtils}
-import streaming.core.datasource.impl.MLSQLDirectJDBC
-import streaming.dsl.auth.{MLSQLTable, OperateType, TableAuthResult, TableType}
+import streaming.core.datasource.JDBCUtils
+import streaming.dsl.auth._
 import streaming.dsl.mmlib.SQLAlg
 import streaming.dsl.mmlib.algs.param.{BaseParams, WowParams}
 import streaming.dsl.{ConnectMeta, DBMappingKey, ScriptSQLExec}
 import tech.mlsql.dsl.auth.ETAuth
 import tech.mlsql.dsl.auth.dsl.mmlib.ETMethod.ETMethod
-import tech.mlsql.sql.MLSQLSQLParser
 
+import java.util.Properties
 import scala.collection.JavaConverters._
 
 
 /**
-  * Created by allwefantasy on 25/8/2018.
-  */
+ * Created by allwefantasy on 25/8/2018.
+ */
 class SQLJDBC(override val uid: String) extends SQLAlg with ETAuth with Functions with WowParams {
   def this() = this(BaseParams.randomUID())
 
   def executeInDriver(options: Map[String, String]) = {
-    val driver = options("driver")
-    val url = options("url")
+    val driver = options.getOrElse("driver", options.getOrElse("conn.driver", ""))
+    val url = options.getOrElse("url", options.getOrElse("conn.url", ""))
+
+    require(!driver.isEmpty && !url.isEmpty, "driver/url should be both specified")
+
     Class.forName(driver)
-    val connection = java.sql.DriverManager.getConnection(url, options("user"), options("password"))
+
+    val properties = new Properties()
+    options.filter(_._1.startsWith("conn.")).foreach { case (k, v) =>
+      properties.put(k.stripPrefix("conn."), v)
+    }
+    properties.put("user", options.getOrElse("user", options.getOrElse("conn.user", "")))
+    properties.put("password", options.getOrElse("password", options.getOrElse("conn.password", "")))
+
+    val connection = java.sql.DriverManager.getConnection(url, properties)
     try {
       // we suppose that there is only one create if
-      val statements = options.filter(f =>"""driver\-statement\-[0-9]+""".r.findFirstMatchIn(f._1).nonEmpty).
+      val statements = options.filter(f => """driver\-statement\-[0-9]+""".r.findFirstMatchIn(f._1).nonEmpty).
         map(f => (f._1.split("-").last.toInt, f._2)).toSeq.sortBy(f => f._1).map(f => f._2).map { f =>
         logInfo(s"${getClass.getName} execute: ${f}")
         connection.prepareStatement(f)
@@ -125,53 +131,22 @@ class SQLJDBC(override val uid: String) extends SQLAlg with ETAuth with Function
   final val sqlMode: Param[String] = new Param[String](this, "sqlMode", "query/ddl default:ddl")
   final val driverStatement: Param[String] = new Param[String](this, "driver-statement-[group]", "DDL you wanna run")
 
-  override def auth(etMethod: ETMethod ,path :String, params: Map[String, String]): List[TableAuthResult] = {
+  override def auth(etMethod: ETMethod, path: String, params: Map[String, String]): List[TableAuthResult] = {
     val context = ScriptSQLExec.contextGetOrForTest()
-
-    params.get(sqlMode.name).
-      map(m => set(sqlMode, m)).getOrElse {
-      // we should be compatible with preview version.
-      set(sqlMode, "ddl")
+    val opTpe = params.get(sqlMode.name) match {
+      case Some("ddl") => OperateType.INSERT
+      case _ => OperateType.SELECT
     }
-
-    var _params = params
-    if (path.contains(".")) {
-      val Array(db, table) = path.split("\\.", 2)
-      ConnectMeta.presentThenCall(DBMappingKey("jdbc", db), options => {
-        options.foreach { item =>
-          _params += (item._1 -> item._2)
-        }
-      })
-    }
-
-    val si = new MLSQLDirectJDBC().sourceInfo(DataAuthConfig("" ,_params))
-
-    var mlsqlTables = List.empty[MLSQLTable]
-    $(sqlMode) match {
-      case "ddl" =>
-      //todo
-      case "query" =>
-        params.get("driver-statement-query").map { sql => {
-            val tableRefs = MLSQLAuthParser.filterTables(sql, context.execListener.sparkSession)
-            val tableList = tableRefs.map(_.identifier).toList
-            val tableColsMap = JDBCUtils.queryTableWithColumnsInDriver(_params ,tableList)
-            val createSqlList = JDBCUtils.tableColumnsToCreateSql(tableColsMap)
-
-            val tableAndCols = MLSQLSQLParser.extractTableWithColumns(si.sourceType ,sql ,createSqlList)
-
-            tableAndCols.foreach {
-              case (table, cols) =>
-                mlsqlTables ::= MLSQLTable(Option(si.db), Option(table), Option(cols.toSet), OperateType.DIRECT_QUERY, Option(si.sourceType), TableType.JDBC)
-            }
-          }
-        }.getOrElse {
-          throw new MLSQLException("driver-statement-query is required")
-        }
-    }
+    val vtable = MLSQLTable(
+      Option(DB_DEFAULT.MLSQL_SYSTEM.toString),
+      Option("JDBC"),
+      opTpe,
+      Option("select"),
+      TableType.SYSTEM)
 
     context.execListener.getTableAuth match {
       case Some(tableAuth) =>
-        tableAuth.auth(mlsqlTables)
+        tableAuth.auth(List(vtable))
       case None => List(TableAuthResult(true, ""))
     }
   }
