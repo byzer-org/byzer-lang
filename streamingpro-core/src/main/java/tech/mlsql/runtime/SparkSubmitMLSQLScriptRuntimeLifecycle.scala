a1f001c7ff01c2ac52b495ce722f8f28a1fd9ec9
WilliamZhu <allwefantasy@gmail.com>
2022-12-06 09:12:10 +0800

    issue-1884 (#1886)

--- streamingpro-core/src/main/java/tech/mlsql/runtime/SparkSubmitMLSQLScriptRuntimeLifecycle.scala
+++ streamingpro-core/src/main/java/tech/mlsql/runtime/SparkSubmitMLSQLScriptRuntimeLifecycle.scala
@@ -29,22 +29,27 @@ class SparkSubmitMLSQLScriptRuntimeLifecycle extends MLSQLPlatformLifecycle with
     JobManager.init(rootSparkSession)
     try {
       val sql = new StringBuffer()
-      if (!mlsql_path.startsWith("http")) {
+      if (mlsql_path.startsWith("file://")) {
+        val file = Source.fromFile(mlsql_path)
+        for (line <- file.getLines()) {
+          sql.append(line).append("\n")
+        }
+      } else if (mlsql_path.startsWith("http://")) {
+        val file = Source.fromURL(mlsql_path)
+        for (line <- file.getLines()) {
+          sql.append(line).append("\n")
+        }
+      } else {
         val array = rootSparkSession.sparkContext.textFile(mlsql_path).collect()
         array.foreach(
           line => {
             sql.append(line).append("\n")
           }
         )
-      } else {
-        val file = Source.fromURL(mlsql_path)
-        for (line <- file.getLines()) {
-          sql.append(line).append("\n")
-        }
       }
 
       if (StringUtils.isEmpty(sql.toString)) {
-        logWarning(s"The mlsql script file is empty.")
+        logWarning(s"The Byzer script file is empty.")
         return
       }
 
